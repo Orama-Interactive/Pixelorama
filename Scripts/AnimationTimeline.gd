2d829d51e961c54c55f7dcacbfedb7c7d2d31762
Schweini07 <47503977+Schweini07@users.noreply.github.com>
2020-04-15 14:47:51 +0200

    Update AnimationTimeline.gd

--- Scripts/AnimationTimeline.gd
+++ Scripts/AnimationTimeline.gd
@@ -87,6 +87,47 @@ func _on_DeleteFrame_pressed():
 	Global.undo_redo.add_undo_method(Global, "undo", [canvas])
 	Global.undo_redo.commit_action()
 
+func _on_CopyFrame_pressed():
+	var canvas : Canvas = Global.canvases[Global.current_frame]
+	var new_canvas : Canvas = load("res://Prefabs/Canvas.tscn").instance()
+	new_canvas.size = Global.canvas.size
+	new_canvas.frame = Global.canvases.size()
+	
+	var new_canvases := Global.canvases.duplicate()
+	new_canvases.append(new_canvas)
+	var new_hidden_canvases := Global.hidden_canvases.duplicate()
+	new_hidden_canvases.append(new_canvas)
+	
+	for layer in canvas.layers: # Copy every layer
+		var sprite := Image.new()
+		sprite.copy_from(layer[0])
+		sprite.lock()
+		var tex := ImageTexture.new()
+		tex.create_from_image(sprite, 0)
+		new_canvas.layers.append([sprite, tex, layer[2]])
+
+		Global.undos += 1
+		Global.undo_redo.create_action("Add Frame")
+		Global.undo_redo.add_do_method(Global, "redo", [new_canvas])
+		Global.undo_redo.add_undo_method(Global, "undo", [new_canvas])
+
+		Global.undo_redo.add_do_property(Global, "canvases", new_canvases)
+		Global.undo_redo.add_do_property(Global, "hidden_canvases", Global.hidden_canvases)
+		Global.undo_redo.add_do_property(Global, "canvas", new_canvas)
+		Global.undo_redo.add_do_property(Global, "current_frame", new_canvases.size() - 1)
+		for i in range(Global.layers.size()):
+			for child in Global.layers[i][3].get_children():
+				Global.undo_redo.add_do_property(child, "pressed", false)
+				Global.undo_redo.add_undo_property(child, "pressed", child.pressed)
+		for c in Global.canvases:
+			Global.undo_redo.add_do_property(c, "visible", false)
+			Global.undo_redo.add_undo_property(c, "visible", c.visible)
+	
+		Global.undo_redo.add_undo_property(Global, "canvases", Global.canvases)
+		Global.undo_redo.add_undo_property(Global, "hidden_canvases", new_hidden_canvases)
+		Global.undo_redo.add_undo_property(Global, "canvas", Global.canvas)
+		Global.undo_redo.add_undo_property(Global, "current_frame", Global.current_frame)
+		Global.undo_redo.commit_action()
 
 func _on_FrameTagButton_pressed() -> void:
 	Global.tag_dialog.popup_centered()
