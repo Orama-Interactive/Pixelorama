fc03e833774c265b7ab9fb4333e50c5421933721
OverloadedOrama <35376950+OverloadedOrama@users.noreply.github.com>
2020-06-05 16:14:57 +0300

    Change the selection rectangle when switching projects

--- src/Classes/Project.gd
+++ src/Classes/Project.gd
@@ -91,6 +91,17 @@ func change_project() -> void:
 
 	self.animation_tags = animation_tags
 
+	if selected_pixels.size() != 0:
+		Global.selection_rectangle.polygon[0] = Vector2(x_min, y_min)
+		Global.selection_rectangle.polygon[1] = Vector2(x_max, y_min)
+		Global.selection_rectangle.polygon[2] = Vector2(x_max, y_max)
+		Global.selection_rectangle.polygon[3] = Vector2(x_min, y_max)
+	else:
+		Global.selection_rectangle.polygon[0] = Vector2.ZERO
+		Global.selection_rectangle.polygon[1] = Vector2.ZERO
+		Global.selection_rectangle.polygon[2] = Vector2.ZERO
+		Global.selection_rectangle.polygon[3] = Vector2.ZERO
+
 
 func frames_changed(value : Array) -> void:
 	frames = value
