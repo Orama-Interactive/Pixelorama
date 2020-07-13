fd3afbfebc6dd48cb790cd846c121d79bac759f0
Kinwailo <lokinwai@gmail.com>
2020-07-13 20:10:17 +0800

    Add overwrite option to pencil. (#282)

--- src/Tools/Pencil.gd
+++ src/Tools/Pencil.gd
@@ -3,26 +3,53 @@ extends "res://src/Tools/Draw.gd"
 
 var _last_position := Vector2.INF
 var _changed := false
+var _overwrite := false
 
 
-class AlphaBlendOp extends Drawer.ColorOp:
+class PencilOp extends Drawer.ColorOp:
 	var changed := false
+	var overwrite := false
 
 
 	func process(src: Color, dst: Color) -> Color:
 		changed = true
 		src.a *= strength
+		if overwrite:
+			return src
 		return dst.blend(src)
 
 
 func _init() -> void:
-	_drawer.color_op = AlphaBlendOp.new()
+	_drawer.color_op = PencilOp.new()
+
+
+func _on_Overwrite_toggled(button_pressed : bool):
+	_overwrite = button_pressed
+	update_config()
+	save_config()
+
+
+func get_config() -> Dictionary:
+	var config := .get_config()
+	config["overwrite"] = _overwrite
+	return config
+
+
+func set_config(config : Dictionary) -> void:
+	.set_config(config)
+	_overwrite = config.get("overwrite", _overwrite)
+
+
+func update_config() -> void:
+	.update_config()
+	$Overwrite.pressed = _overwrite
 
 
 func draw_start(position : Vector2) -> void:
 	update_mask()
 	_changed = false
 	_drawer.color_op.changed = false
+	_drawer.color_op.overwrite = _overwrite
 
 	prepare_undo()
 	_drawer.reset()
@@ -65,4 +92,7 @@ func draw_end(_position : Vector2) -> void:
 
 func _draw_brush_image(image : Image, src_rect: Rect2, dst: Vector2) -> void:
 	_changed = true
-	_get_draw_image().blend_rect(image, src_rect, dst)
+	if _overwrite:
+		_get_draw_image().blit_rect(image, src_rect, dst)
+	else:
+		_get_draw_image().blend_rect(image, src_rect, dst)
