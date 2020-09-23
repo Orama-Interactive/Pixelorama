c0db420f73a10d90017c3aefbf7051c3dd95c77b
OverloadedOrama <35376950+OverloadedOrama@users.noreply.github.com>
2020-09-23 15:28:03 +0300

    Update splash screen with 3 new artworks
    Also change copyright notice in splash screen & about dialog

--- src/UI/Dialogs/SplashDialog.gd
+++ src/UI/Dialogs/SplashDialog.gd
@@ -1,10 +1,20 @@
 extends WindowDialog
 
 
+var artworks := {
+	"Roroto Sic" : [preload("res://assets/graphics/splash_screen/artworks/roroto.png"), "https://www.instagram.com/roroto_sic/"],
+	"jess.mpz" : [preload("res://assets/graphics/splash_screen/artworks/jessmpz.png"), "https://www.instagram.com/jess.mpz/"],
+	"Wishdream" : [preload("res://assets/graphics/splash_screen/artworks/wishdream.png"), "https://twitter.com/WishdreamStar"]
+}
+
+var chosen_artwork = ""
+
+
 func _on_SplashDialog_about_to_show() -> void:
+	var splash_art_texturerect : TextureRect = Global.find_node_by_name(self, "SplashArt")
 	var art_by_label : Button = Global.find_node_by_name(self, "ArtistName")
 	var show_on_startup_button : CheckBox = Global.find_node_by_name(self, "ShowOnStartup")
-	var developed_by_label : Label = Global.find_node_by_name(self, "DevelopedBy")
+	var copyright_label : Label = Global.find_node_by_name(self, "CopyrightLabel")
 	var become_platinum : Button = Global.find_node_by_name(self, "BecomePlatinum")
 	var become_gold : Button = Global.find_node_by_name(self, "BecomeGold")
 	var become_patron : Button = Global.find_node_by_name(self, "BecomePatron")
@@ -13,16 +23,21 @@ func _on_SplashDialog_about_to_show() -> void:
 		show_on_startup_button.pressed = !Global.config_cache.get_value("preferences", "startup")
 	window_title = "Pixelorama" + " " + Global.current_version
 
-	art_by_label.text = tr("Art by: %s") % "Wishdream"
+	chosen_artwork = artworks.keys()[randi() % artworks.size()]
+	splash_art_texturerect.texture = artworks[chosen_artwork][0]
+
+	art_by_label.text = tr("Art by: %s") % chosen_artwork
+	art_by_label.hint_tooltip = artworks[chosen_artwork][1]
+
 	become_platinum.text = "- " + tr("Become a Platinum Sponsor")
 	become_gold.text = "- " + tr("Become a Gold Sponsor")
 	become_patron.text = "- " + tr("Become a Patron")
 	if "zh" in TranslationServer.get_locale():
 		show_on_startup_button.add_font_override("font", preload("res://assets/fonts/CJK/NotoSansCJKtc-Small.tres"))
-		developed_by_label.add_font_override("font", preload("res://assets/fonts/CJK/NotoSansCJKtc-Small.tres"))
+		copyright_label.add_font_override("font", preload("res://assets/fonts/CJK/NotoSansCJKtc-Small.tres"))
 	else:
 		show_on_startup_button.add_font_override("font", preload("res://assets/fonts/Roboto-Small.tres"))
-		developed_by_label.add_font_override("font", preload("res://assets/fonts/Roboto-Small.tres"))
+		copyright_label.add_font_override("font", preload("res://assets/fonts/Roboto-Small.tres"))
 
 	get_stylebox("panel", "WindowDialog").bg_color = Global.control.theme.get_stylebox("panel", "WindowDialog").bg_color
 	get_stylebox("panel", "WindowDialog").border_color = Global.control.theme.get_stylebox("panel", "WindowDialog").border_color
@@ -32,7 +47,7 @@ func _on_SplashDialog_about_to_show() -> void:
 
 
 func _on_ArtCredits_pressed() -> void:
-	OS.shell_open("https://twitter.com/WishdreamStar")
+	OS.shell_open(artworks[chosen_artwork][1])
 
 
 func _on_ShowOnStartup_toggled(pressed : bool) -> void:
