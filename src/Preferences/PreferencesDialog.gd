d66f75cefc41ef7cf4a65d1a42235b6eab760e71
Benoit Gschwind <gschwind@gnu-log.net>
2020-11-06 21:37:30 +0100

    Fix the computation of cursor location
    For an unknown reason the function get_local_mouse_position return
    incorrect current_pixel when shrink is not 1. This path compute the
    transformation manually and povide correct values for any shrink value.

--- src/Preferences/PreferencesDialog.gd
+++ src/Preferences/PreferencesDialog.gd
@@ -183,3 +183,5 @@ func _on_ShrinkApplyButton_pressed():
 	get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_DISABLED,
 		SceneTree.STRETCH_ASPECT_IGNORE, Vector2(1014,576), Global.shrink)
 	Global.preferences_dialog.popup_centered(Vector2(400, 280))
+	Global.camera.zoom_100()
+	
