86a640fc228b87971a633e6e58ab0f623b3d9439
Variable <77773850+Variable-ind@users.noreply.github.com>
2021-05-14 02:57:12 +0500

    moved onion skins to their own nodes

---
+++ src/UI/Canvas/OnionPast.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+func _draw() -> void:
+	if Global.onion_skinning:
+		if Global.onion_skinning_past_rate > 0:
+			var color : Color
+			if Global.onion_skinning_blue_red:
+				color = Color.blue
+			else:
+				color = Color.white
+			for i in range(1, Global.onion_skinning_past_rate + 1):
+				if Global.current_project.current_frame >= i:
+					var layer_i := 0
+					for layer in Global.current_project.frames[Global.current_project.current_frame - i].cels:
+						if Global.current_project.layers[layer_i].visible:
+							color.a = 0.6 / i
+							draw_texture(layer.image_texture, Vector2.ZERO, color)
+							update()
+						layer_i += 1
