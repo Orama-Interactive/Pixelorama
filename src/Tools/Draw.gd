6b9f86061886f6969a62a8c6daf7235b7b5a4b52
Manolis Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2021-05-13 21:50:55 +0300

    Fix brushes being drawn flipped horizontally
    My bad!

--- src/Tools/Draw.gd
+++ src/Tools/Draw.gd
@@ -294,11 +294,11 @@ func draw_tool_brush(position : Vector2) -> void:
 
 func remove_unselected_parts_of_brush(brush : Image, dst : Vector2) -> Image:
 	var project : Project = Global.current_project
+	if !project.has_selection:
+		return brush
 	var size := brush.get_size()
 	var new_brush := Image.new()
-	new_brush.copy_from(_mirror_brushes.x)
-	if !project.has_selection:
-		return new_brush
+	new_brush.copy_from(brush)
 
 	new_brush.lock()
 	for x in size.x:
