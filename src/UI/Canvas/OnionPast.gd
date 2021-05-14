76ded2e08a099753a41c817effc337b8617707c7
Variable <77773850+Variable-ind@users.noreply.github.com>
2021-05-14 14:00:28 +0500

    command to ignore onion of layer
    if a layer has "ignore_onion" (CASE IN-SENSITIVE) in its name then that layer will not participate in onion skinning. if onion skinning of layers like "background" were getting in the way simply change its name to "ignore_onion background" or "background ignore_onion" or "IGNORE_ONION background" etc will all work

--- src/UI/Canvas/OnionPast.gd
+++ src/UI/Canvas/OnionPast.gd
@@ -14,7 +14,9 @@ func _draw() -> void:
 					var layer_i := 0
 					for layer in Global.current_project.frames[Global.current_project.current_frame - i].cels:
 						if Global.current_project.layers[layer_i].visible:
-							color.a = 0.6 / i
-							draw_texture(layer.image_texture, Vector2.ZERO, color)
-							update()
+							#ignore layer if it has "onion_ignore" in its name (case in-sensitive).
+							if not "ignore_onion" in Global.current_project.layers[layer_i].name.to_lower():
+								color.a = 0.6 / i
+								draw_texture(layer.image_texture, Vector2.ZERO, color)
+								update()
 						layer_i += 1
