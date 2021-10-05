Orama-Interactive/Pixelorama

commit 86e66495b1d4e7677332f63502383e2d6199dcec

Fix issue with copying and pasting to a different project that has a bigger canvas

src/UI/Canvas/Selection.gd
@@ -632,7 +632,9 @@ func paste() -> void:
 	original_big_bounding_rectangle = big_bounding_rectangle
 	original_offset = project.selection_offset
 
-	project.selection_bitmap = clipboard.selection_bitmap.duplicate()
+	var clip_bitmap : BitMap = clipboard.selection_bitmap.duplicate()
+
+	project.selection_bitmap = Global.current_project.resize_bitmap(clip_bitmap, project.selection_bitmap.get_size())
 	self.big_bounding_rectangle = clipboard.big_bounding_rectangle
 	project.selection_offset = clipboard.selection_offset
 

commit b7e598c438801a8c1ccef3b22ec9068deb5f7b2f

Fix issue with copying and pasting to a different project that has a smaller canvas
Fixes a bug caused by 86e66495b1d4e7677332f63502383e2d6199dcec

src/UI/Canvas/Selection.gd
@@ -633,8 +633,9 @@ func paste() -> void:
 	original_offset = project.selection_offset
 
 	var clip_bitmap : BitMap = clipboard.selection_bitmap.duplicate()
+	var max_size := Vector2(max(clip_bitmap.get_size().x, project.selection_bitmap.get_size().x), max(clip_bitmap.get_size().y, project.selection_bitmap.get_size().y))
 
-	project.selection_bitmap = Global.current_project.resize_bitmap(clip_bitmap, project.selection_bitmap.get_size())
+	project.selection_bitmap = Global.current_project.resize_bitmap(clip_bitmap, max_size)
 	self.big_bounding_rectangle = clipboard.big_bounding_rectangle
 	project.selection_offset = clipboard.selection_offset
 