845bf2bab75149096b613ab0405c09b31c9f3bd8
Manolis Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2021-10-06 01:51:39 +0300

    Fixes critical bug that is caused by removing a project tab that is to the left of the currently active tab

--- src/UI/Tabs.gd
+++ src/UI/Tabs.gd
@@ -44,5 +44,8 @@ func delete_tab(tab : int) -> void:
 			Global.current_project_index -= 1
 		else:
 			Global.current_project_index = 0
+	else:
+		if tab < Global.current_project_index:
+			Global.current_project_index -= 1
 	if Global.unsaved_changes_dialog.is_connected("confirmed", self, "delete_tab"):
 		Global.unsaved_changes_dialog.disconnect("confirmed", self, "delete_tab")
