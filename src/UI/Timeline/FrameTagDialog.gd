304d7d392503f78ceaf66966b99732981522e78f
Variable <77773850+Variable-ind@users.noreply.github.com>
2021-11-16 07:37:23 +0500

    removed randomize() cause it's not needed

--- src/UI/Timeline/FrameTagDialog.gd
+++ src/UI/Timeline/FrameTagDialog.gd
@@ -65,7 +65,6 @@ func _on_AddTag_pressed() -> void:
 	current_tag_id = Global.current_project.animation_tags.size()
 	options_dialog.get_node("GridContainer/FromSpinBox").value = Global.current_project.current_frame + 1
 	options_dialog.get_node("GridContainer/ToSpinBox").value = Global.current_project.current_frame + 1
-	randomize()
 	options_dialog.get_node("GridContainer/ColorPickerButton").color = Color(randf(),randf(),randf())
 
 
