02a74d498c7615bc1b9dfaaaa859f24d8638c870
Manolis Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2021-12-06 19:11:50 +0200

    Fix shape tool previews not being updated when modifier keys are being pressed

--- src/UI/Canvas/Previews.gd
+++ src/UI/Canvas/Previews.gd
@@ -2,8 +2,14 @@ extends Node2D
 
 
 func _input(event: InputEvent) -> void:
-	if Global.has_focus and event is InputEventMouse:
-		update()
+	if Global.has_focus:
+		if (
+			event is InputEventMouse
+			or event.is_action("shift")
+			or event.is_action("ctrl")
+			or event.is_action("alt")
+		):
+			update()
 
 
 func _draw() -> void:
