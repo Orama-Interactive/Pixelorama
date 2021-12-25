6708fb24db8f0d99ae023894147d60e1fe3b3954
Variable <77773850+Variable-ind@users.noreply.github.com>
2021-12-25 21:58:07 +0500

    some minor changes
    spritesheet "Start frame" no longer restricted to existing frames number
    and a minor UI change

--- src/UI/Dialogs/PreviewDialog.gd
+++ src/UI/Dialogs/PreviewDialog.gd
@@ -156,8 +156,6 @@ func _on_ImportOption_item_selected(id: int) -> void:
 		frame_size_label.visible = true
 		spritesheet_tab_options.visible = true
 		spritesheet_layer_options.visible = true
-		var n_of_frames: int = Global.current_project.frames.size()
-		spritesheet_layer_options.get_node("AtFrameSpinbox").max_value = n_of_frames
 		texture_rect.get_child(0).visible = true
 		texture_rect.get_child(1).visible = true
 		rect_size.x = spritesheet_layer_options.rect_size.x
