93268fef43133553125a5218f234a698f63dc954
Manolis Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2021-12-25 19:21:09 +0200

    Fix another bug with Undo/Redo where the app became unresponsive if the user drew outside the canvas

--- src/Tools/Eraser.gd
+++ src/Tools/Eraser.gd
@@ -90,8 +90,8 @@ func draw_end(position: Vector2) -> void:
 		draw_tool(_line_start)
 		draw_fill_gap(_line_start, _line_end)
 		_draw_line = false
-	if _changed or _drawer.color_op.changed:
-		commit_undo()
+
+	commit_undo()
 	cursor_text = ""
 	update_random_image()
 
