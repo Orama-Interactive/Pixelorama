07473bea9521063decac555ca7a5e6811d827f9d
Manolis Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2022-03-06 00:15:25 +0200

    Update UI.gd

--- src/UI/UI.gd
+++ src/UI/UI.gd
@@ -7,7 +7,7 @@ onready var main_canvas_container = Global.main_canvas_container
 func _ready() -> void:
 	update_transparent_shader()
 
-	#Set anchors for ShaderVision
+	# Set anchors for ShaderVision
 	Global.greyscale_vision.visible = false
 	Global.greyscale_vision.anchor_left = ANCHOR_BEGIN
 	Global.greyscale_vision.anchor_top = ANCHOR_BEGIN
@@ -23,8 +23,8 @@ func _on_main_canvas_visibility_changed() -> void:
 	update_transparent_shader()
 
 
-func update_transparent_shader():
-	# works independently of the transparency frature
+func update_transparent_shader() -> void:
+	# Works independently of the transparency feature
 	material.set("shader_param/screen_resolution", get_viewport().size)
 	material.set("shader_param/position", main_canvas_container.rect_global_position)
 	material.set("shader_param/size", main_canvas_container.rect_size)
