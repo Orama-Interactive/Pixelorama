1e0eb19c15d384ffc4052b29b20e4de7c664565e
Emmanouil Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2022-05-15 22:05:35 +0300

    Don't fill color when creating a new empty frame if the color is transparent

--- src/Classes/Project.gd
+++ src/Classes/Project.gd
@@ -111,7 +111,7 @@ func new_empty_frame() -> Frame:
 	for l in layers:  # Create as many cels as there are layers
 		var image := Image.new()
 		image.create(size.x, size.y, false, Image.FORMAT_RGBA8)
-		if bottom_layer:
+		if bottom_layer and fill_color.a > 0:
 			image.fill(fill_color)
 		frame.cels.append(Cel.new(image, 1))
 		bottom_layer = false
