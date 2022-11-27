3be713c713801df6201d18dadb4876598dd6c4a0
Emmanouil Papadeas <manoschool@yahoo.gr>
2022-11-27 02:34:22 +0200

    Fix "Create new folder for each frame tag" being visible even if the CollapsibleContainer's button is not pressed

--- src/UI/Nodes/CollapsibleContainer.gd
+++ src/UI/Nodes/CollapsibleContainer.gd
@@ -8,6 +8,10 @@ export var visible_content := false setget _set_visible_content
 
 func _ready() -> void:
 	_set_visible($Button.pressed)
+	for child in get_children():
+		if not child is CanvasItem or child == $Button:
+			continue
+		child.connect("visibility_changed", self, "_child_visibility_changed", [child])
 
 
 func _set_text(value: String) -> void:
@@ -30,6 +34,13 @@ func _set_visible(pressed: bool) -> void:
 	else:
 		$Button/TextureRect.rect_rotation = -90
 	for child in get_children():
-		if child == $Button:
+		if not child is CanvasItem or child == $Button:
 			continue
 		child.visible = pressed
+
+
+# Checks if a child becomes visible from another sure and ensures
+# it remains invisible if the button is not pressed
+func _child_visibility_changed(child: CanvasItem) -> void:
+	if not $Button.pressed:
+		child.visible = false
