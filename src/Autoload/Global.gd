4989e3e33a2db7b76e10ce498c98e9c2ff97ddfc
Emmanouil Papadeas <manoschool@yahoo.gr>
2022-12-02 01:15:49 +0200

    Fix Global not being able to find the Reference Images panel

--- src/Autoload/Global.gd
+++ src/Autoload/Global.gd
@@ -176,7 +176,7 @@ onready var brushes_popup: Popup = control.find_node("BrushesPopup")
 onready var patterns_popup: Popup = control.find_node("PatternsPopup")
 onready var palette_panel: PalettePanel = control.find_node("Palettes")
 
-onready var references_panel: ReferencesPanel = control.find_node("References")
+onready var references_panel: ReferencesPanel = control.find_node("Reference Images")
 
 onready var top_menu_container: Panel = control.find_node("TopMenuContainer")
 onready var rotation_level_button: Button = control.find_node("RotationLevel")
