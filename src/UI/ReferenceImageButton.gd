4f7486defb0557ecc84b3a6a2b38e78302217eb3
Variable <77773850+Variable-ind@users.noreply.github.com>
2022-12-08 05:20:54 +0500

    Reference UI improvements (#789)
    * Added the filter parameter
    
    * Improved the reference image panel UI
    
    * Disable Os open path for HTML
    
    * fix typo
    
    * Aviod crash on image not found

--- src/UI/ReferenceImageButton.gd
+++ src/UI/ReferenceImageButton.gd
@@ -6,7 +6,17 @@ var _ignore_spinbox_changes = false
 
 
 func _ready():
-	$Interior/Path.text = element.image_path
+	if OS.get_name() == "HTML5":
+		$Interior/PathHeader/Path.visible = false
+		$Interior/PathHeader/PathHTML.text = element.image_path
+	else:
+		$Interior/PathHeader/PathHTML.visible = false
+		$Interior/PathHeader/Path.text = element.image_path
+
+	if !element.texture:
+		$Interior/PreviewAndOptions/PreviewPanel/Warning.text = "Image Not Found!!!"
+	else:
+		$Interior/PreviewAndOptions/PreviewPanel/Preview.texture = element.texture
 	element.connect("properties_changed", self, "_update_properties")
 	_update_properties()
 
@@ -15,12 +25,13 @@ func _update_properties():
 	# This is because otherwise a little dance will occur.
 	# This also breaks non-uniform scales (not supported UI-wise, but...)
 	_ignore_spinbox_changes = true
-	$Interior/Options/Scale.value = element.scale.x * 100
-	$Interior/Options/X.value = element.position.x
-	$Interior/Options/Y.value = element.position.y
-	$Interior/Options/X.max_value = element.project.size.x
-	$Interior/Options/Y.max_value = element.project.size.y
-	$Interior/Options2/Opacity.value = element.modulate.a * 100
+	$Interior/PreviewAndOptions/Options/Scale.value = element.scale.x * 100
+	$Interior/PreviewAndOptions/Options/Position/X.value = element.position.x
+	$Interior/PreviewAndOptions/Options/Position/Y.value = element.position.y
+	$Interior/PreviewAndOptions/Options/Position/X.max_value = element.project.size.x
+	$Interior/PreviewAndOptions/Options/Position/Y.max_value = element.project.size.y
+	$Interior/PreviewAndOptions/Options/Opacity.value = element.modulate.a * 100
+	$Interior/OtherOptions/ApplyFilter.pressed = element.filter
 	_ignore_spinbox_changes = false
 
 
@@ -64,4 +75,18 @@ func _on_Opacity_value_changed(value):
 	if _ignore_spinbox_changes:
 		return
 	element.modulate.a = value / 100
+	element.change_properties()
+
+
+func _on_Path_pressed() -> void:
+	OS.shell_open($Interior/PathHeader/Path.text.get_base_dir())
+
+
+func _on_ApplyFilter_toggled(button_pressed: bool) -> void:
+	element.filter = button_pressed
+	if element.texture:
+		if element.filter:
+			element.texture.flags = Texture.FLAG_MIPMAPS | Texture.FLAG_FILTER
+		else:
+			element.texture.flags = Texture.FLAG_MIPMAPS
 	element.change_properties()
