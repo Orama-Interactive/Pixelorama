0e024f6d3d6b50278dced468f56155af047de7c2
Variable <77773850+Variable-ind@users.noreply.github.com>
2022-12-28 21:35:02 +0500

    Allow floating point for VRuler

--- src/UI/Canvas/Rulers/VerticalRuler.gd
+++ src/UI/Canvas/Rulers/VerticalRuler.gd
@@ -73,7 +73,7 @@ func _draw() -> void:
 			var text_xform = Transform2D(-PI / 2, Vector2(font.get_height() - 4, position.y - 2))
 			draw_set_transform_matrix(get_transform() * text_xform)
 			var val = (ruler_transform * major_subdivide * minor_subdivide).xform(Vector2(0, j)).y
-			draw_string(font, Vector2(), str(int(val)))
+			draw_string(font, Vector2(), str(stepify(val, 0.1)))
 			draw_set_transform_matrix(get_transform())
 		else:
 			if j % minor_subdivision == 0:
