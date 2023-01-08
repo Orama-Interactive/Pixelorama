97513bae38e475f19c5fbf3e28ab45985f934c88
Variable <77773850+Variable-ind@users.noreply.github.com>
2023-01-08 23:58:53 +0500

    formatting i forgot to do

--- src/UI/PerspectiveEditor/VanishingPoint.gd
+++ src/UI/PerspectiveEditor/VanishingPoint.gd
@@ -47,7 +47,7 @@ func initiate(start_data = null, idx = -1) -> void:
 		update_data_to_project()
 
 
-func update_boundary_color(color :Color):
+func update_boundary_color(color: Color):
 	var luminance = (0.2126 * color.r) + (0.7152 * color.g) + (0.0722 * color.b)
 	color.a = 0.9 - luminance * 0.4  # Interpolates between 0.5 to 0.9
 	boundary_l.color = color
