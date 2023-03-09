1892aed11fa71e20cd31f30fd829dbb0a7fb1393
MrTriPie <MrTriPie>
2023-03-09 12:13:45 -0500

    Cleaned up CropRect._draw

--- src/UI/Canvas/CropRect.gd
+++ src/UI/Canvas/CropRect.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-const LINE_COLOR = Color.white
+const RECT_COLOR = Color.white
 
 var top := 0
 var bottom := 0
@@ -8,9 +8,4 @@ var left := 0
 var right := 0
 
 func _draw():
-	# Horizontal lines, top to bottom:
-	draw_line(Vector2(left, top), Vector2(right, top), LINE_COLOR)
-	draw_line(Vector2(left, bottom), Vector2(right, bottom), LINE_COLOR)
-	# Vertical lines, left to right:
-	draw_line(Vector2(left, top), Vector2(left, bottom), LINE_COLOR)
-	draw_line(Vector2(right, top), Vector2(right, bottom), LINE_COLOR)
+	draw_rect(Rect2(left, top, (right - left), (bottom - top)), RECT_COLOR, false)
