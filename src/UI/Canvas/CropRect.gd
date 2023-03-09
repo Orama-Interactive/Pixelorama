3b11efceebcdb5ab6ad7b15d0a87da2b4dee7719
MrTriPie <MrTriPie>
2023-03-09 10:19:24 -0500

    [skip ci] Removed unneeeded LINE_WIDTH constant from CropRect

--- src/UI/Canvas/CropRect.gd
+++ src/UI/Canvas/CropRect.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-const WIDTH = 1
 const LINE_COLOR = Color.white
 
 var top := 0
@@ -10,8 +9,8 @@ var right := 0
 
 func _draw():
 	# Horizontal lines, top to bottom:
-	draw_line(Vector2(left, top), Vector2(right, top), LINE_COLOR, WIDTH)
-	draw_line(Vector2(left, bottom), Vector2(right, bottom), LINE_COLOR, WIDTH)
+	draw_line(Vector2(left, top), Vector2(right, top), LINE_COLOR)
+	draw_line(Vector2(left, bottom), Vector2(right, bottom), LINE_COLOR)
 	# Vertical lines, left to right:
-	draw_line(Vector2(left, top), Vector2(left, bottom), LINE_COLOR, WIDTH)
-	draw_line(Vector2(right, top), Vector2(right, bottom), LINE_COLOR, WIDTH)
+	draw_line(Vector2(left, top), Vector2(left, bottom), LINE_COLOR)
+	draw_line(Vector2(right, top), Vector2(right, bottom), LINE_COLOR)
