f15e79c14f71e30a44466d509024f4319c19e9af
MrTriPie <MrTriPie>
2023-03-10 23:18:51 -0500

    Added new modes, refactored, added rule of thirds lines and darkened background

--- src/Tools/Crop.gd
+++ src/Tools/Crop.gd
@@ -1,71 +1,190 @@
 extends BaseTool
 # Crop Tool, allows you to resize the canvas interactively
 
+# TODO: Rename CropTool
+
 var _crop_rect: CropRect
 var _start_pos: Vector2
+var _syncing := false
 
-func _ready():
+func _ready() -> void:
 	_crop_rect = Global.canvas.crop_rect
-	_crop_rect.connect("updated", self, "_rect_updated")
+	_crop_rect.connect("updated", self, "_sync_ui")
 	_crop_rect.tool_count += 1
-	_rect_updated()
+	_sync_ui()
 
 
-func _exit_tree():
+func _exit_tree() -> void:
 	_crop_rect.tool_count -= 1
 
 
-func _rect_updated():
+func _sync_ui() -> void:
+	_syncing = true
+	$"%CropMode".selected = _crop_rect.mode
+
+	match _crop_rect.mode:
+		CropRect.Mode.SIDES:
+			$"%SidesContainer".show()
+			$"%RatioContainer".hide()
+			$"%ResolutionContainer".hide()
+			$"%DimensionsLabel".show()
+		CropRect.Mode.RESOLUTION, CropRect.Mode.LOCKED_RESOLUTION:
+			$"%SidesContainer".hide()
+			$"%RatioContainer".hide()
+			$"%ResolutionContainer".show()
+			$"%DimensionsLabel".hide()
+		CropRect.Mode.LOCKED_ASPECT_RATIO:
+			$"%SidesContainer".hide()
+			$"%RatioContainer".show()
+			$"%ResolutionContainer".show()
+			$"%DimensionsLabel".hide()
+
 	$"%Top".max_value = Global.current_project.size.y - 1
 	$"%Bottom".max_value = Global.current_project.size.y
 	$"%Left".max_value = Global.current_project.size.x - 1
 	$"%Right".max_value = Global.current_project.size.x
-	$"%Top".value = _crop_rect.top
-	$"%Bottom".value = _crop_rect.bottom
-	$"%Left".value = _crop_rect.left
-	$"%Right".value = _crop_rect.right
-	$"%DimensionsLabel".text = str(_crop_rect.right - _crop_rect.left, " x ", _crop_rect.bottom - _crop_rect.top)
+	$"%Top".value = _crop_rect.rect.position.y
+	$"%Bottom".value = _crop_rect.rect.end.y
+	$"%Left".value = _crop_rect.rect.position.x
+	$"%Right".value = _crop_rect.rect.end.x
+
+	$"%RatioX".value = _crop_rect.ratio.x
+	$"%RatioY".value = _crop_rect.ratio.y
+
+	$"%PositionX".max_value = Global.current_project.size.x - 1
+	$"%PositionY".max_value = Global.current_project.size.y - 1
+	$"%Width".max_value = Global.current_project.size.x
+	$"%Height".max_value = Global.current_project.size.y
+	$"%PositionX".value = _crop_rect.rect.position.x
+	$"%PositionY".value = _crop_rect.rect.position.y
+	$"%Width".value = _crop_rect.rect.size.x
+	$"%Height".value = _crop_rect.rect.size.y
+
+	$"%DimensionsLabel".text = str(_crop_rect.rect.size.x, " x ", _crop_rect.rect.size.y)
+	_syncing = false
 
 
 func draw_start(position: Vector2) -> void:
 	.draw_start(position)
 	_start_pos = position
+	if _crop_rect.mode == CropRect.Mode.LOCKED_RESOLUTION:
+		_crop_rect.rect.position = position
+		_crop_rect.emit_signal("updated")
 
 
 func draw_move(position: Vector2) -> void:
 	.draw_move(position)
-	_crop_rect.top = min(_start_pos.y, position.y)
-	_crop_rect.bottom = max(_start_pos.y, position.y)
-	_crop_rect.left = min(_start_pos.x, position.x)
-	_crop_rect.right = max(_start_pos.x, position.x)
+	match _crop_rect.mode:
+		CropRect.Mode.SIDES, CropRect.Mode.RESOLUTION:
+			_crop_rect.rect.position.x = min(_start_pos.x, position.x)
+			_crop_rect.rect.position.y = min(_start_pos.y, position.y)
+			_crop_rect.rect.end.x = max(_start_pos.x, position.x)
+			_crop_rect.rect.end.y = max(_start_pos.y, position.y)
+		CropRect.Mode.LOCKED_RESOLUTION:
+			_crop_rect.rect.position = position
+		CropRect.Mode.LOCKED_ASPECT_RATIO:
+			_crop_rect.rect.position.x = min(_start_pos.x, position.x)
+			_crop_rect.rect.position.y = min(_start_pos.y, position.y)
+			var width := abs(_start_pos.x - position.x)
+			var height := abs(_start_pos.y - position.y)
+			_crop_rect.rect.size.x = max(width, height * _crop_rect.ratio.x / _crop_rect.ratio.y)
+			_crop_rect.rect.size.y = max(height, width * _crop_rect.ratio.y / _crop_rect.ratio.x)
 	_crop_rect.emit_signal("updated")
 
 
 # UI Signals:
 
+func _on_CropMode_item_selected(index: int) -> void:
+	if _syncing:
+		return
+	_crop_rect.mode = index
+	_crop_rect.emit_signal("updated")
+
+
 func _on_Top_value_changed(value: float) -> void:
-	_crop_rect.top = value
-	_crop_rect.bottom = max(_crop_rect.top + 1, _crop_rect.bottom)
+	if _syncing:
+		return
+	_crop_rect.rect.position.y = value
+	# TODO: Fix this:
+	_crop_rect.rect.end.y = max(_crop_rect.rect.position.y + 1, _crop_rect.rect.end.y)
 	_crop_rect.emit_signal("updated")
 
 
 func _on_Bottom_value_changed(value: float) -> void:
-	_crop_rect.bottom = value
-	_crop_rect.top = min(_crop_rect.bottom - 1, _crop_rect.top)
+	if _syncing:
+		return
+	_crop_rect.rect.end.y = value
+	_crop_rect.rect.position.y = min(_crop_rect.rect.end.y - 1, _crop_rect.rect.position.y)
 	_crop_rect.emit_signal("updated")
 
 
 func _on_Left_value_changed(value: float) -> void:
-	_crop_rect.left = value
-	_crop_rect.right = max(_crop_rect.left + 1, _crop_rect.right)
+	if _syncing:
+		return
+	_crop_rect.rect.position.x = value
+	_crop_rect.rect.end.x = max(_crop_rect.rect.position.x + 1, _crop_rect.rect.end.x)
 	_crop_rect.emit_signal("updated")
 
 
 func _on_Right_value_changed(value: float) -> void:
-	_crop_rect.right = value
-	_crop_rect.left = min(_crop_rect.right - 1, _crop_rect.left)
+	if _syncing:
+		return
+	_crop_rect.rect.end.x = value
+	_crop_rect.rect.position.x = min(_crop_rect.rect.end.x - 1, _crop_rect.rect.position.x)
 	_crop_rect.emit_signal("updated")
 
 
-func _on_Apply_pressed():
+func _on_RatioX_value_changed(value: float) -> void:
+	if _syncing:
+		return
+	_crop_rect.rect.size.x = _crop_rect.rect.size.y / _crop_rect.ratio.y * value
+	_crop_rect.ratio.x = value
+	_crop_rect.emit_signal("updated")
+
+
+func _on_RatioY_value_changed(value: float) -> void:
+	if _syncing:
+		return
+	_crop_rect.rect.size.y = _crop_rect.rect.size.x / _crop_rect.ratio.x * value
+	_crop_rect.ratio.y = value
+	_crop_rect.emit_signal("updated")
+
+
+func _on_PositionX_value_changed(value: float) -> void:
+	if _syncing:
+		return
+	_crop_rect.rect.position.x = value
+	_crop_rect.emit_signal("updated")
+
+
+func _on_PositionY_value_changed(value: float) -> void:
+	if _syncing:
+		return
+	_crop_rect.rect.position.y = value
+	_crop_rect.emit_signal("updated")
+
+
+func _on_Width_value_changed(value: float) -> void:
+	if _syncing:
+		return
+	if _crop_rect.mode == CropRect.Mode.LOCKED_ASPECT_RATIO:
+		# TODO: May need to be rounded to int
+		# TODO: May need to have a min value of 1 set using max(..., 1)
+		_crop_rect.rect.size.y = (value / _crop_rect.ratio.x) * _crop_rect.ratio.y
+	_crop_rect.rect.size.x = value
+	_crop_rect.emit_signal("updated")
+
+
+func _on_Height_value_changed(value: float) -> void:
+	if _syncing:
+		return
+	if _crop_rect.mode == CropRect.Mode.LOCKED_ASPECT_RATIO:
+		# TODO: May need to be rounded to int
+		# TODO: May need to have a min value of 1 set using max(..., 1)
+		_crop_rect.rect.size.x = (value / _crop_rect.ratio.y) * _crop_rect.ratio.x
+	_crop_rect.rect.size.y = value
+	_crop_rect.emit_signal("updated")
+
+
+func _on_Apply_pressed() -> void:
 	_crop_rect.apply()
--- src/UI/Canvas/CropRect.gd
+++ src/UI/Canvas/CropRect.gd
@@ -1,45 +1,85 @@
-class_name CropRect
+class_name CropRect # TODO: Rename to just Crop
 extends Node2D
 # Draws the rectangle overlay for the crop tool
 # Stores the shared settings between left and right crop tools
 
+enum Mode { SIDES, RESOLUTION, LOCKED_RESOLUTION, LOCKED_ASPECT_RATIO }
+
 signal updated
 
-const RECT_COLOR = Color.white
+const BIG = 100000 # Size of big rectangles used to darken background.
+const DARKEN_COLOR = Color(0, 0, 0, 0.5)
+const LINE_COLOR = Color.white
 
-var top := 0
-var bottom := 0
-var left := 0
-var right := 0
+var mode := 0 setget _set_mode
+var rect := Rect2(0, 0, 1, 1)
+var ratio := Vector2.ONE
 
 # How many crop tools are active (0-2), setter makes this visible if not 0
 var tool_count := 0 setget _set_tool_count
 
 
-func _ready():
+func _ready() -> void:
 	connect("updated", self, "update")
 	Global.connect("project_changed", self, "reset")
 	reset()
 
 
-func _draw():
-	draw_rect(Rect2(left, top, (right - left), (bottom - top)), RECT_COLOR, false)
+func _draw() -> void:
+	# Darken the background by drawing very big rectangles around the crop rect:
+	draw_rect(Rect2(rect.end.x - BIG, rect.position.y - BIG, BIG, BIG), DARKEN_COLOR)
+	draw_rect(Rect2(rect.end.x, rect.end.y - BIG, BIG, BIG), DARKEN_COLOR)
+	draw_rect(Rect2(rect.position.x, rect.end.y, BIG, BIG), DARKEN_COLOR)
+	draw_rect(Rect2(rect.position.x - BIG, rect.position.y, BIG, BIG), DARKEN_COLOR)
+
+	# Rect:
+	draw_rect(rect, LINE_COLOR, false)
+
+	# Horizontal rule of thirds lines:
+	var third: float = rect.position.y + rect.size.y * 0.333
+	draw_line(Vector2(rect.position.x, third), Vector2(rect.end.x, third), LINE_COLOR)
+	third = rect.position.y + rect.size.y * 0.667
+	draw_line(Vector2(rect.position.x, third), Vector2(rect.end.x, third), LINE_COLOR)
+
+	# Vertical rule of thirds lines:
+	third = rect.position.x + rect.size.x * 0.333
+	draw_line(Vector2(third, rect.position.y), Vector2(third, rect.end.y), LINE_COLOR)
+	third = rect.position.x + rect.size.x * 0.667
+	draw_line(Vector2(third, rect.position.y), Vector2(third, rect.end.y), LINE_COLOR)
 
 
-func apply():
-	DrawingAlgos.resize_canvas((right - left), (bottom - top), -left, -top)
+func apply() -> void:
+	DrawingAlgos.resize_canvas(rect.size.x, rect.size.y, -rect.position.x, -rect.position.y)
 
-
-func reset():
-	top = 0
-	bottom = Global.current_project.size.y
-	left = 0
-	right = Global.current_project.size.x
+# TODO: Reset needs to be called when opening a project (that is the current and project_changed isn't emitted?)
+func reset() -> void:
+	rect.position = Vector2.ZERO
+	rect.size = Global.current_project.size
+	_auto_ratio_from_resolution()
 	emit_signal("updated")
 
 
-func _set_tool_count(value: int):
+func _auto_ratio_from_resolution() -> void:
+	var divisor := _gcd(rect.size.x, rect.size.y)
+	ratio = rect.size / divisor
+
+
+# Greatest common divisor
+func _gcd(a: int, b: int) -> int:
+	return a if b == 0 else _gcd(b, a % b)
+
+
+# Setters
+
+func _set_mode(value: int) -> void:
+	if value == Mode.LOCKED_ASPECT_RATIO and mode != Mode.LOCKED_ASPECT_RATIO:
+		_auto_ratio_from_resolution()
+	mode = value
+
+
+func _set_tool_count(value: int) -> void:
 	if tool_count == 0 and value > 0:
 		reset() # Reset once 1 tool becomes the crop tool
 	tool_count = value
 	visible = tool_count
+
