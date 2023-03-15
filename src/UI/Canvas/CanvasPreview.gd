2f584dd68287ab66c0876b6938442762d59f3e54
Variable <77773850+Variable-ind@users.noreply.github.com>
2023-03-15 15:21:51 +0500

    spritesheet preview

--- src/UI/Canvas/CanvasPreview.gd
+++ src/UI/Canvas/CanvasPreview.gd
@@ -1,58 +1,122 @@
 extends Node2D
 
+enum Mode { TIMELINE, SPRITESHEET }
+var mode = Mode.TIMELINE
+
+var h_frames: int = 1
+var v_frames: int = 1
+var start_sprite_sheet_frame: int = 1
+var end_sprite_sheet_frame: int = 1
+var sprite_frames = []
+
 var frame: int = 0
+
 onready var animation_timer: Timer = $AnimationTimer
 
 
 func _draw() -> void:
 	var current_project: Project = Global.current_project
-	if frame >= current_project.frames.size():
-		frame = current_project.current_frame
+	var texture_to_draw := ImageTexture.new()
+	var modulate_color := Color.white
+	match mode:
+		Mode.TIMELINE:
+			if frame >= current_project.frames.size():
+				frame = current_project.current_frame
 
-	$AnimationTimer.wait_time = (
-		current_project.frames[frame].duration
-		* (1 / Global.current_project.fps)
-	)
+			$AnimationTimer.wait_time = (
+				current_project.frames[frame].duration
+				* (1 / Global.current_project.fps)
+			)
 
-	if animation_timer.is_stopped():
-		frame = current_project.current_frame
-	var current_cels: Array = current_project.frames[frame].cels
+			if animation_timer.is_stopped():
+				frame = current_project.current_frame
+			var current_cels: Array = current_project.frames[frame].cels
 
-	# Draw current frame layers
-	for i in range(current_cels.size()):
-		if current_cels[i] is GroupCel:
-			continue
-		var modulate_color := Color(1, 1, 1, current_cels[i].opacity)
-		if (
-			i < current_project.layers.size()
-			and current_project.layers[i].is_visible_in_hierarchy()
-		):
-			draw_texture(current_cels[i].image_texture, Vector2.ZERO, modulate_color)
+			# Draw current frame layers
+			for i in range(current_cels.size()):
+				if current_cels[i] is GroupCel:
+					continue
+				modulate_color = Color(1, 1, 1, current_cels[i].opacity)
+				if (
+					i < current_project.layers.size()
+					and current_project.layers[i].is_visible_in_hierarchy()
+				):
+					texture_to_draw = current_cels[i].image_texture
+		Mode.SPRITESHEET:
+			var target_frame = current_project.frames[current_project.current_frame]
+			var frame_image = Image.new()
+			frame_image.create(
+				current_project.size.x,
+				current_project.size.y,
+				false,
+				Image.FORMAT_RGBA8
+			)
+			Export.blend_all_layers(frame_image, target_frame)
+			sprite_frames = split_spritesheet(frame_image, h_frames, v_frames)
+
+			# limit start and end
+			if end_sprite_sheet_frame > sprite_frames.size():
+				end_sprite_sheet_frame = sprite_frames.size()
+			if start_sprite_sheet_frame < 0:
+				start_sprite_sheet_frame = 0
+			# reset frame if required
+			if frame >= end_sprite_sheet_frame:
+				frame = start_sprite_sheet_frame - 1
+			texture_to_draw = sprite_frames[frame]
+
+	var rect = Rect2(Vector2.ZERO, texture_to_draw.get_data().get_size())
+	get_parent().get_node("TransparentChecker").fit_rect(rect)
+	draw_texture(texture_to_draw, Vector2.ZERO, modulate_color)
 
 
 func _on_AnimationTimer_timeout() -> void:
-	var first_frame := 0
-	var last_frame: int = Global.current_project.frames.size() - 1
-	var current_project: Project = Global.current_project
+	match mode:
+		Mode.TIMELINE:
+			var first_frame := 0
+			var last_frame: int = Global.current_project.frames.size() - 1
+			var current_project: Project = Global.current_project
 
-	if Global.play_only_tags:
-		for tag in current_project.animation_tags:
-			if (
-				current_project.current_frame + 1 >= tag.from
-				&& current_project.current_frame + 1 <= tag.to
-			):
-				first_frame = tag.from - 1
-				last_frame = min(current_project.frames.size() - 1, tag.to - 1)
+			if Global.play_only_tags:
+				for tag in current_project.animation_tags:
+					if (
+						current_project.current_frame + 1 >= tag.from
+						&& current_project.current_frame + 1 <= tag.to
+					):
+						first_frame = tag.from - 1
+						last_frame = min(current_project.frames.size() - 1, tag.to - 1)
 
-	if frame < last_frame:
-		frame += 1
-	else:
-		frame = first_frame
+			if frame < last_frame:
+				frame += 1
+			else:
+				frame = first_frame
 
+			$AnimationTimer.wait_time = (
+				Global.current_project.frames[frame].duration
+				* (1 / Global.current_project.fps)
+			)
+
+		Mode.SPRITESHEET:
+			frame += 1
+			$AnimationTimer.wait_time = (1 / Global.current_project.fps)
 	$AnimationTimer.set_one_shot(true)
-	$AnimationTimer.wait_time = (
-		Global.current_project.frames[frame].duration
-		* (1 / Global.current_project.fps)
-	)
 	$AnimationTimer.start()
 	update()
+
+
+func split_spritesheet(image: Image, horiz: int, vert: int) -> Array:
+	var result = []
+	horiz = min(horiz, image.get_size().x)
+	vert = min(vert, image.get_size().y)
+	var frame_width := image.get_size().x / horiz
+	var frame_height := image.get_size().y / vert
+	for yy in range(vert):
+		for xx in range(horiz):
+			var tex := ImageTexture.new()
+			var cropped_image := Image.new()
+			cropped_image = image.get_rect(
+				Rect2(frame_width * xx, frame_height * yy, frame_width, frame_height)
+			)
+			cropped_image.convert(Image.FORMAT_RGBA8)
+			tex.create_from_image(cropped_image, 0)
+			result.append(tex)
+	return result
--- src/UI/CanvasPreviewContainer/CanvasPreviewContainer.gd
+++ src/UI/CanvasPreviewContainer/CanvasPreviewContainer.gd
@@ -1,8 +1,13 @@
 extends PanelContainer
 
-onready var canvas_preview = $HBoxContainer/PreviewViewportContainer/Viewport/CanvasPreview
-onready var camera: Camera2D = $HBoxContainer/PreviewViewportContainer/Viewport/CameraPreview
-onready var play_button: Button = $HBoxContainer/VBoxContainer/PlayButton
+onready var canvas_preview: Node2D = $"%CanvasPreview"
+onready var camera: Camera2D = $"%CameraPreview"
+onready var play_button: Button = $"%PlayButton"
+
+onready var h_frames: SpinBox = $"%HFrames"
+onready var v_frames: SpinBox = $"%VFrames"
+onready var start: SpinBox = $"%Start"
+onready var end: SpinBox = $"%End"
 
 
 func _on_PreviewZoomSlider_value_changed(value: float) -> void:
@@ -13,11 +18,55 @@ func _on_PreviewZoomSlider_value_changed(value: float) -> void:
 
 func _on_PlayButton_toggled(button_pressed: bool) -> void:
 	if button_pressed:
-		if Global.current_project.frames.size() <= 1:
-			play_button.pressed = false
-			return
+		if canvas_preview.mode == canvas_preview.Mode.TIMELINE:
+			if Global.current_project.frames.size() <= 1:
+				play_button.pressed = false
+				return
+		else:
+			if start.value == end.value:
+				play_button.pressed = false
+				return
 		canvas_preview.animation_timer.start()
 		Global.change_button_texturerect(play_button.get_child(0), "pause.png")
 	else:
 		canvas_preview.animation_timer.stop()
 		Global.change_button_texturerect(play_button.get_child(0), "play.png")
+
+
+func _on_OptionButton_item_selected(index: int) -> void:
+	play_button.pressed = false
+	canvas_preview.mode = index
+	$VBox/Animation/VBoxContainer/Options.visible = bool(index == 1)
+	canvas_preview.update()
+
+
+func _on_HFrames_value_changed(value: float) -> void:
+	canvas_preview.h_frames = value
+	var frames = canvas_preview.h_frames * canvas_preview.v_frames
+	start.max_value = frames
+	end.max_value = frames
+	canvas_preview.update()
+
+
+func _on_VFrames_value_changed(value: float) -> void:
+	canvas_preview.v_frames = value
+	var frames = canvas_preview.h_frames * canvas_preview.v_frames
+	start.max_value = frames
+	end.max_value = frames
+	canvas_preview.update()
+
+
+func _on_Start_value_changed(value: float) -> void:
+	canvas_preview.frame = value - 1
+	canvas_preview.start_sprite_sheet_frame = value
+	if end.value < value:
+		end.value = value
+	canvas_preview.update()
+
+
+func _on_End_value_changed(value: float) -> void:
+	canvas_preview.end_sprite_sheet_frame = value
+	if start.value > value:
+		start.value = value
+		canvas_preview.frame = value - 1
+	canvas_preview.update()
