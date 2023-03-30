Orama-Interactive/Pixelorama

commit c2f6bf0f3f2ec1fad8e0fff38cad2a2d6052b4b4

Fix errors when attempting to draw on a 3D cel
Can occur when multiple cels are selected, some of them 3D and some of them pixel

src/Tools/BaseTool.gd
@@ -260,8 +260,9 @@ func _get_selected_draw_images() -> Array:  # Array of Images
 	var project: Project = Global.current_project
 	for cel_index in project.selected_cels:
 		var cel: BaseCel = project.frames[cel_index[0]].cels[cel_index[1]]
-		if project.layers[cel_index[1]].can_layer_get_drawn():
-			images.append(cel.get_image())
+		if not cel is PixelCel:
+			continue
+		images.append(cel.get_image())
 	return images
 
 

src/UI/Canvas/Selection.gd
@@ -653,8 +653,7 @@ func _get_selected_draw_images() -> Array:  # Array of Image(s)
 		var cel: BaseCel = project.frames[cel_index[0]].cels[cel_index[1]]
 		if not cel is PixelCel:
 			continue
-		if project.layers[cel_index[1]].can_layer_get_drawn():
-			images.append(cel.image)
+		images.append(cel.image)
 	return images
 
 

commit 0c7f6840074ff5a5c5af76b62d4388cd8d4072b3

Fix bug where locked/invisible layers could get drawn
Regression from c2f6bf0f3f2ec1fad8e0fff38cad2a2d6052b4b4

src/Tools/BaseTool.gd
@@ -262,7 +262,8 @@ func _get_selected_draw_images() -> Array:  # Array of Images
 		var cel: BaseCel = project.frames[cel_index[0]].cels[cel_index[1]]
 		if not cel is PixelCel:
 			continue
-		images.append(cel.get_image())
+		if project.layers[cel_index[1]].can_layer_get_drawn():
+			images.append(cel.get_image())
 	return images
 
 

src/UI/Canvas/Selection.gd
@@ -653,7 +653,8 @@ func _get_selected_draw_images() -> Array:  # Array of Image(s)
 		var cel: BaseCel = project.frames[cel_index[0]].cels[cel_index[1]]
 		if not cel is PixelCel:
 			continue
-		images.append(cel.image)
+		if project.layers[cel_index[1]].can_layer_get_drawn():
+			images.append(cel.get_image())
 	return images
 
 