390eca554c451e0cfcaf2f4b97c32cf11cea72c4
Emmanouil Papadeas <manoschool@yahoo.gr>
2023-05-09 16:39:35 +0300

    Remove the default cube from 3D cels

--- src/Classes/Cel3D.gd
+++ src/Classes/Cel3D.gd
@@ -38,8 +38,6 @@ func _init(_size: Vector2, from_pxo := false, _object_prop := {}, _scene_prop :=
 			transform.origin = Vector3(-2.5, 0, 0)
 			object_properties[0] = {"type": Cel3DObject.Type.DIR_LIGHT, "transform": transform}
 			_add_object_node(0)
-			object_properties[1] = {"type": Cel3DObject.Type.BOX}
-			_add_object_node(1)
 		current_object_id = object_properties.size()
 
 
