32e0406fd0cf650eb5b48cc678934e29d39152c8
Variable <pkfayezakhtar@gmail.com>
2023-06-21 12:06:14 +0500

    formatting

--- src/UI/Nodes/AnimatePanel.gd
+++ src/UI/Nodes/AnimatePanel.gd
@@ -1,17 +1,17 @@
 class_name AnimatePanel
 extends PanelContainer
 
+var frames := []  # Set this value before calling "get_animated_values"
+var properties := []  # Contains dictionary of properties
+var resetter_values := []  # Contains the Original properties without any change
+var _current_id: int = 0  # The property currently selected in "property_list"
+
+var image_effect_node: ConfirmationDialog
 onready var can_animate_button: CheckBox = $"%CanAnimate"
 onready var property_list: ItemList = $"%PropertyList"
 onready var initial_value: ValueSlider = $"%Initial"
 onready var final_value: ValueSlider = $"%Final"
 onready var preview_slider: TextureProgress = $"%PreviewSlider"
-var image_effect_node: ConfirmationDialog
-
-var frames := []  # Set this value before calling "get_animated_values"
-var properties := []  # Contains dictionary of properties
-var resetter_values := []  # Contains the Original properties without any change
-var _current_id: int = 0  # The property currently selected in "property_list"
 
 
 func _ready() -> void:
