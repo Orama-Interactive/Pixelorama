b8646118ac938d8da6f79475acb8cc588531d5a7
Emmanouil Papadeas <manoschool@yahoo.gr>
2023-07-11 22:20:21 +0300

    Remove old .tres font files

--- src/UI/Canvas/Rulers/Guide.gd
+++ src/UI/Canvas/Rulers/Guide.gd
@@ -5,7 +5,7 @@ enum Types { HORIZONTAL, VERTICAL }
 
 const INPUT_WIDTH := 4
 
-var font := preload("res://assets/fonts/Roboto-Regular.tres")
+var font := preload("res://assets/fonts/Roboto-Regular.ttf")
 var has_focus := true
 var mouse_pos := Vector2.ZERO
 var type: int = Types.HORIZONTAL
