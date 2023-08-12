3c8c72401dd801b1845cc25a84297662c5f4da99
Emmanouil Papadeas <manoschool@yahoo.gr>
2023-08-12 13:35:06 +0300

    Release v0.11.1-stable

--- src/UI/TopMenuContainer/TopMenuContainer.gd
+++ src/UI/TopMenuContainer/TopMenuContainer.gd
@@ -730,7 +730,7 @@ func help_menu_id_pressed(id: int) -> void:
 			OS.shell_open(ProjectSettings.globalize_path("user://logs"))
 		Global.HelpMenu.CHANGELOG:
 			OS.shell_open(
-				"https://github.com/Orama-Interactive/Pixelorama/blob/master/CHANGELOG.md#v011---2023-06-13"
+				"https://github.com/Orama-Interactive/Pixelorama/blob/master/CHANGELOG.md#v0111---2023-08-12"
 			)
 		Global.HelpMenu.ABOUT_PIXELORAMA:
 			_popup_dialog(Global.control.get_node("Dialogs/AboutDialog"))
