5afbc1a4ec9c45126fa3031c5b2a6fa311283c8f
OverloadedOrama <manoschool@yahoo.gr>
2023-08-20 21:45:35 +0300

    Attempt to fix #901

--- src/UI/Tabs.gd
+++ src/UI/Tabs.gd
@@ -49,11 +49,11 @@ func _on_Tabs_reposition_active_tab_request(idx_to: int) -> void:
 
 	# Change save paths
 	var temp_save_path = OpenSave.current_save_paths[Global.current_project_index]
-	OpenSave.current_save_paths[Global.current_project_index] = OpenSave.current_save_paths[idx_to]
-	OpenSave.current_save_paths[idx_to] = temp_save_path
+	OpenSave.current_save_paths.remove(Global.current_project_index)
+	OpenSave.current_save_paths.insert(idx_to, temp_save_path)
 	var temp_backup_path = OpenSave.backup_save_paths[Global.current_project_index]
-	OpenSave.backup_save_paths[Global.current_project_index] = OpenSave.backup_save_paths[idx_to]
-	OpenSave.backup_save_paths[idx_to] = temp_backup_path
+	OpenSave.backup_save_paths.remove(Global.current_project_index)
+	OpenSave.backup_save_paths.insert(idx_to, temp_save_path)
 
 
 func delete_tab(tab: int) -> void:
