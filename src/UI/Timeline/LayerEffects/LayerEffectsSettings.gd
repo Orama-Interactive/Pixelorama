3e74785ceef96a0df7774caa98bc0fa80edb53eb
Emmanouil Papadeas <manoschool@yahoo.gr>
2023-11-06 21:38:23 +0200

    Swap effects

--- src/UI/Timeline/LayerEffects/LayerEffectsSettings.gd
+++ src/UI/Timeline/LayerEffects/LayerEffectsSettings.gd
@@ -1,6 +1,8 @@
 extends AcceptDialog
 
 const DELETE_TEXTURE := preload("res://assets/graphics/layers/delete.png")
+const MOVE_DOWN_TEXTURE := preload("res://assets/graphics/layers/move_down.png")
+const MOVE_UP_TEXTURE := preload("res://assets/graphics/layers/move_up.png")
 
 var effects: Array[LayerEffect] = [
 	LayerEffect.new("Outline", preload("res://src/Shaders/OutlineInline.gdshader")),
@@ -25,7 +27,7 @@ func _ready() -> void:
 func _on_about_to_popup() -> void:
 	var layer := Global.current_project.layers[Global.current_project.current_layer]
 	for effect in layer.effects:
-		_create_effect_ui(effect)
+		_create_effect_ui(layer, effect)
 
 
 func _on_visibility_changed() -> void:
@@ -36,30 +38,58 @@ func _on_visibility_changed() -> void:
 
 
 func _on_effect_list_id_pressed(index: int) -> void:
+	var layer := Global.current_project.layers[Global.current_project.current_layer]
 	var effect := effects[index].duplicate()
-	_create_effect_ui(effect)
-	Global.current_project.layers[Global.current_project.current_layer].effects.append(effect)
+	_create_effect_ui(layer, effect)
+	layer.effects.append(effect)
 	Global.canvas.queue_redraw()
 
 
-func _create_effect_ui(effect: LayerEffect) -> void:
+func _create_effect_ui(layer: BaseLayer, effect: LayerEffect) -> void:
 	var hbox := HBoxContainer.new()
 	var enable_checkbox := CheckBox.new()
 	enable_checkbox.button_pressed = effect.enabled
+	enable_checkbox.mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND
 	enable_checkbox.toggled.connect(_enable_effect.bind(effect))
 	var label := Label.new()
 	label.text = effect.name
 	var delete_button := TextureButton.new()
+	delete_button.mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND
 	delete_button.texture_normal = DELETE_TEXTURE
 	delete_button.pressed.connect(_delete_effect.bind(effect))
+	var move_up_button := TextureButton.new()
+	move_up_button.mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND
+	move_up_button.texture_normal = MOVE_UP_TEXTURE
+	move_up_button.pressed.connect(_re_order_effect.bind(effect, layer, hbox, -1))
+	var move_down_button := TextureButton.new()
+	move_down_button.mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND
+	move_down_button.texture_normal = MOVE_DOWN_TEXTURE
+	move_down_button.pressed.connect(_re_order_effect.bind(effect, layer, hbox, 1))
 	hbox.add_child(enable_checkbox)
 	hbox.add_child(label)
 	hbox.add_child(delete_button)
+	hbox.add_child(move_up_button)
+	hbox.add_child(move_down_button)
 	effect_container.add_child(hbox)
 
 
 func _enable_effect(button_pressed: bool, effect: LayerEffect) -> void:
 	effect.enabled = button_pressed
+	Global.canvas.queue_redraw()
+
+
+func _re_order_effect(effect: LayerEffect, layer: BaseLayer, container: HBoxContainer, direction: int) -> void:
+	assert(layer.effects.size() == effect_container.get_child_count())
+	var effect_index := container.get_index()
+	var new_index := effect_index + direction
+	if new_index < 0:
+		return
+	if new_index >= effect_container.get_child_count():
+		return
+	effect_container.move_child(container, new_index)
+	var temp := layer.effects[new_index]
+	layer.effects[new_index] = effect
+	layer.effects[effect_index] = temp
 	Global.canvas.queue_redraw()
 
 
