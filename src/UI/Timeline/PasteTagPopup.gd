c7617807d119e7b25cfc403dff1a8e0cb4d36d11
Variable <pkfayezakhtar@gmail.com>
2023-11-22 10:07:27 +0500

    improved code

--- src/UI/Timeline/PasteTagPopup.gd
+++ src/UI/Timeline/PasteTagPopup.gd
@@ -60,16 +60,13 @@ func _on_TagList_id_pressed(id: int) -> void:
 	hide()
 
 
-# gets frame indices of from_project and dumps it in current_project
+## Gets frame indices of [member from_project] and dumps it in the current project.
 func add_animation(indices: Array, destination: int):
 	var project: Project = Global.current_project
-	if from_project == project:
+	if from_project == project:  ## If we are copying tags within project
 		Global.animation_timeline.copy_frames(indices, destination)
 		return
 
-	var copied_frames := []
-	# the indices of newly copied frames
-	var copied_indices := range(destination + 1, (destination + 1) + indices.size())
 	var new_animation_tags := project.animation_tags.duplicate()
 	# Loop through the tags to create new classes for them, so that they won't be the same
 	# as project.animation_tags's classes. Needed for undo/redo to work properly.
@@ -80,69 +77,75 @@ func add_animation(indices: Array, destination: int):
 			new_animation_tags[i].from,
 			new_animation_tags[i].to
 		)
+	var imported_frames: Array[Frame] = []  # The copied frames
+	# the indices of newly copied frames
+	var copied_indices: PackedInt32Array = range(
+		destination + 1, (destination + 1) + indices.size()
+	)
 	project.undos += 1
 	project.undo_redo.create_action("Import Tag")
 	# Step 1: calculate layers to generate
-	var from_l_names := PackedStringArray()
-	for l in from_project.layers:
-		from_l_names.append(l.name)
-	var to_l_names := PackedStringArray()
+	var layer_to_names := PackedStringArray()  # names of currently existing layers
 	for l in project.layers:
-		to_l_names.append(l.name)
-	var targets := PackedInt32Array()  # target layers the content will be copied to
-	var search_idx = 0
-	for i in from_l_names.size():
-		var idx = to_l_names.find(from_l_names[i], search_idx)
-		if project.layers[idx].get_layer_type() != from_project.layers[i].get_layer_type():
-			idx = -1
-		else:
-			search_idx = idx + 1
-			idx += targets.count(-1)
-		targets.append(idx)
+		layer_to_names.append(l.name)
+
+	# the goal of this section is to mark existing layers with their indices else with -1
+	var layer_from_to := {}  # indices of layers from and to
+	for from in from_project.layers.size():
+		var to = layer_to_names.find(from_project.layers[from].name)
+		if project.layers[to].get_layer_type() != from_project.layers[from].get_layer_type():
+			to = -1
+		if to in layer_from_to.values():  # from_project has layers with duplicate frames
+			to = -1
+		layer_from_to[from] = to
+
 	# Step 2: generate required layers
-	var combined_copy := Array()
+	var combined_copy := Array()  # Makes calculations easy
 	combined_copy.append_array(project.layers)
 	var added_layers := Array()  # Array of layers
-	var added_idx := Array()  # Array of layers
-	var added_cels := Array()  # Array of added cels
-	for i in targets.size():
-		if targets[i] == -1:
-			var current_layer_idx = 0
-			if i > 0:
-				current_layer_idx = targets[i - 1] + 1
-			var type = from_project.layers[i].get_layer_type()
-			var current_layer = combined_copy[current_layer_idx]
-			var l: BaseLayer
-			match type:
-				Global.LayerTypes.PIXEL:
-					l = PixelLayer.new(project)
-				Global.LayerTypes.GROUP:
-					l = GroupLayer.new(project)
-				Global.LayerTypes.THREE_D:
-					l = Layer3D.new(project)
+	var added_idx := Array()  # Array of indices to add the respective layers (in added_layers) to
+	var added_cels := Array()  # Array of an Array of cels (added in same order as their layer)
 
-			var cels := []
-			for f in project.frames:
-				cels.append(l.new_empty_cel())
+	if layer_from_to.values().count(-1) > 0:
+		# As it is extracted from a dictionary, so i assume the keys aren't sorted
+		var from_layers_size = layer_from_to.keys().duplicate(true)
+		from_layers_size.sort()  # it's values should now be from (layer size - 1) to zero
+		for i in from_layers_size:
+			if layer_from_to[i] == -1:
+				var type = from_project.layers[i].get_layer_type()
+				var current_layer = combined_copy[-1]
+				var l: BaseLayer
+				match type:
+					Global.LayerTypes.PIXEL:
+						l = PixelLayer.new(project)
+					Global.LayerTypes.GROUP:
+						l = GroupLayer.new(project)
+					Global.LayerTypes.THREE_D:
+						l = Layer3D.new(project)
+				var cels := []
+				for f in project.frames:
+					cels.append(l.new_empty_cel())
 
-			var new_layer_idx = current_layer_idx
-			l.parent = current_layer.parent
-			l.name = from_l_names[targets.find(-1)]  # this will set it to the required layer name
-			targets[i] = new_layer_idx
-			added_layers.append(l)
-			added_idx.append(new_layer_idx)
-			added_cels.append(cels)
-			combined_copy.insert(new_layer_idx, l)
+				var new_layer_idx = combined_copy.size()
+				l.parent = current_layer.parent
+				l.name = from_project.layers[i].name  # this will set it to the required layer name
+				layer_from_to[i] = new_layer_idx
+				combined_copy.insert(new_layer_idx, l)
+				added_layers.append(l)
+				added_idx.append(new_layer_idx)
+				added_cels.append(cels)
+
 	# Now initiate import
 	for f in indices:
 		var src_frame: Frame = from_project.frames[f]
 		var new_frame := Frame.new()
-		copied_frames.append(new_frame)
+		imported_frames.append(new_frame)
 		new_frame.duration = src_frame.duration
-		for l in combined_copy.size():
+		for to in combined_copy.size():
 			var new_cel: BaseCel
-			if l in targets:
-				var src_cel: BaseCel = from_project.frames[f].cels[targets.find(l)]  # Cel we're copying from, the source
+			if to in layer_from_to.values():
+				var from = layer_from_to.find_key(to)
+				var src_cel: BaseCel = from_project.frames[f].cels[from]  # Cel we're copying from, the source
 				var selected_id := -1
 				if src_cel is Cel3D:
 					new_cel = src_cel.get_script().new(
@@ -170,7 +173,7 @@ func add_animation(indices: Array, destination: int):
 							if selected_id != -1:
 								new_cel.selected = new_cel.get_object_from_id(selected_id)
 			else:
-				new_cel = combined_copy[l].new_empty_cel()
+				new_cel = combined_copy[to].new_empty_cel()
 			new_frame.cels.append(new_cel)
 
 		for tag in new_animation_tags:  # Loop through the tags to see if the frame is in one
@@ -179,14 +182,13 @@ func add_animation(indices: Array, destination: int):
 			elif copied_indices[0] < tag.from:
 				tag.from += 1
 				tag.to += 1
+	project.undo_redo.add_undo_method(project.remove_frames.bind(copied_indices))
 	project.undo_redo.add_do_method(project.add_layers.bind(added_layers, added_idx, added_cels))
-	project.undo_redo.add_do_method(Global.undo_or_redo.bind(false))
-	project.undo_redo.add_undo_method(Global.undo_or_redo.bind(true))
-		# Note: temporarily set the selected cels to an empty array (needed for undo/redo)
+	project.undo_redo.add_do_method(project.add_frames.bind(imported_frames, copied_indices))
+	project.undo_redo.add_undo_method(project.remove_layers.bind(added_idx))
+	# Note: temporarily set the selected cels to an empty array (needed for undo/redo)
 	project.undo_redo.add_do_property(Global.current_project, "selected_cels", [])
 	project.undo_redo.add_undo_property(Global.current_project, "selected_cels", [])
-	project.undo_redo.add_do_method(project.add_frames.bind(copied_frames, copied_indices))
-	project.undo_redo.add_undo_method(project.remove_frames.bind(copied_indices))
 
 	var all_new_cels = []
 	# Select all the new frames so that it is easier to move/offset collectively if user wants
@@ -197,31 +199,15 @@ func add_animation(indices: Array, destination: int):
 	if frame_diff_sign == 0:
 		frame_diff_sign = 1
 	for i in range(range_start, range_end + frame_diff_sign, frame_diff_sign):
-		for j in range(0, Global.current_project.layers.size()):
+		for j in range(0, combined_copy.size()):
 			var frame_layer := [i, j]
 			if !all_new_cels.has(frame_layer):
 				all_new_cels.append(frame_layer)
 	project.undo_redo.add_do_property(Global.current_project, "selected_cels", all_new_cels)
+	project.undo_redo.add_undo_method(project.change_cel.bind(project.current_frame, project.current_layer))
 	project.undo_redo.add_do_method(project.change_cel.bind(range_end))
-#	project.undo_redo.add_do_method(project.change_cel.bind(copied_indices[0]))
-	project.undo_redo.add_undo_method(project.change_cel.bind(project.current_frame))
 	project.undo_redo.add_do_property(project, "animation_tags", new_animation_tags)
 	project.undo_redo.add_undo_property(project, "animation_tags", project.animation_tags)
-	project.undo_redo.add_undo_method(project.remove_layers.bind(added_idx))
+	project.undo_redo.add_do_method(Global.undo_or_redo.bind(false))
+	project.undo_redo.add_undo_method(Global.undo_or_redo.bind(true))
 	project.undo_redo.commit_action()
-#	# Select all the new frames so that it is easier to move/offset collectively if user wants
-#	# To ease animation workflow, new current frame is the first copied frame instead of the last
-#	var range_start: int = copied_indices[-1]
-#	var range_end = copied_indices[0]
-#	var frame_diff_sign = sign(range_end - range_start)
-#	if frame_diff_sign == 0:
-#		frame_diff_sign = 1
-#	for i in range(range_start, range_end + frame_diff_sign, frame_diff_sign):
-#		for j in range(0, Global.current_project.layers.size()):
-#			var frame_layer := [i, j]
-#			if !Global.current_project.selected_cels.has(frame_layer):
-#				Global.current_project.selected_cels.append(frame_layer)
-#	Global.current_project.change_cel(range_end, -1)
-#	await get_tree().process_frame
-#	await get_tree().process_frame
-#	Global.animation_timeline.adjust_scroll_container()
