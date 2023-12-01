743969b9f8125c48ff5065c05bf8d7e59a27cd56
Emmanouil Papadeas <manoschool@yahoo.gr>
2023-12-02 01:03:47 +0200

    Make the new color picker fully functional

--- src/UI/ColorPickers/ColorPickers.gd
+++ src/UI/ColorPickers/ColorPickers.gd
@@ -1,16 +1,16 @@
 extends Container
 
-@onready var left_picker := $ColorPickersHorizontal/LeftColorPickerButton as ColorPickerButton
-@onready var right_picker := $ColorPickersHorizontal/RightColorPickerButton as ColorPickerButton
-@onready var average_color := $"%AverageColor" as ColorRect
-@onready var color_picker: ColorPicker = $ColorPicker
+var picking_color := MOUSE_BUTTON_LEFT
+
+@onready var average_color := %AverageColor as ColorRect
+@onready var color_picker := %ColorPicker as ColorPicker
+@onready var right_color_rect := %RightColorRect as ColorRect
+@onready var left_color_rect := %LeftColorRect as ColorRect
 
 
 func _ready() -> void:
 	Tools.color_changed.connect(update_color)
-	left_picker.get_picker().presets_visible = false
-	right_picker.get_picker().presets_visible = false
-	_average(left_picker.color, right_picker.color)
+	_average(left_color_rect.color, right_color_rect.color)
 
 	await get_tree().process_frame
 	var picker_margin_container := color_picker.get_child(0, true) as MarginContainer
@@ -25,51 +25,47 @@ func _ready() -> void:
 	var sampler_cont := picker_vbox_container.get_child(1, true) as HBoxContainer
 	var color_texture_rect := sampler_cont.get_child(1, true) as TextureRect
 	color_texture_rect.visible = false
-	var hex_cont :=  picker_vbox_container.get_child(4, true).get_child(1, true) as Container
+	var hex_cont := picker_vbox_container.get_child(4, true).get_child(1, true) as Container
 	var hex_edit := hex_cont.get_child(2, true)
 	hex_cont.remove_child(hex_edit)
 	sampler_cont.add_child(hex_edit)
 	sampler_cont.move_child(hex_edit, 1)
 
 
+func _on_color_picker_color_changed(color: Color) -> void:
+	if picking_color == MOUSE_BUTTON_RIGHT:
+		right_color_rect.color = color
+	else:
+		left_color_rect.color = color
+	Tools.assign_color(color, picking_color)
+
+
+func _on_left_color_button_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		picking_color = MOUSE_BUTTON_LEFT
+		color_picker.color = left_color_rect.color
+	else:
+		picking_color = MOUSE_BUTTON_RIGHT
+		color_picker.color = right_color_rect.color
+	_average(left_color_rect.color, right_color_rect.color)
+
+
+func update_color(color: Color, button: int) -> void:
+	if picking_color == button:
+		color_picker.color = color
+	if button == MOUSE_BUTTON_RIGHT:
+		right_color_rect.color = color
+	else:
+		left_color_rect.color = color
+	_average(left_color_rect.color, right_color_rect.color)
+
+
 func _on_ColorSwitch_pressed() -> void:
 	Tools.swap_color()
 
 
-func _on_ColorPickerButton_color_changed(color: Color, right: bool):
-	var button := MOUSE_BUTTON_RIGHT if right else MOUSE_BUTTON_LEFT
-	Tools.assign_color(color, button)
-	_average(left_picker.color, right_picker.color)
-
-
-func _on_ToLeft_pressed():
-	Tools.assign_color(average_color.color, MOUSE_BUTTON_LEFT)
-
-
-func _on_ToRight_pressed():
-	Tools.assign_color(average_color.color, MOUSE_BUTTON_RIGHT)
-
-
-func _on_ColorPickerButton_pressed() -> void:
-	Global.can_draw = false
-	Tools.color_changed.disconnect(update_color)
-
-
-func _on_ColorPickerButton_popup_closed() -> void:
-	Global.can_draw = true
-	Tools.color_changed.connect(update_color)
-
-
 func _on_ColorDefaults_pressed() -> void:
 	Tools.default_color()
-
-
-func update_color(color: Color, button: int) -> void:
-	if button == MOUSE_BUTTON_LEFT:
-		left_picker.color = color
-	else:
-		right_picker.color = color
-	_average(left_picker.color, right_picker.color)
 
 
 func _average(color_1: Color, color_2: Color) -> void:
@@ -80,7 +76,7 @@ func _average(color_1: Color, color_2: Color) -> void:
 	var average := Color(average_r, average_g, average_b, average_a)
 
 	var copy_button = average_color.get_parent()
-	copy_button.tooltip_text = str("Average Color:\n#", average.to_html(), "\n(Press to Copy)")
+	copy_button.tooltip_text = str("Average Color:\n#", average.to_html())
 	average_color.color = average
 
 
@@ -90,3 +86,10 @@ func _on_CopyAverage_button_down():
 
 func _on_CopyAverage_button_up():
 	average_color.visible = true
+
+
+func _on_copy_average_gui_input(event: InputEvent) -> void:
+	if event.is_action_released(&"left_mouse"):
+		Tools.assign_color(average_color.color, MOUSE_BUTTON_LEFT)
+	elif event.is_action_released(&"right_mouse"):
+		Tools.assign_color(average_color.color, MOUSE_BUTTON_RIGHT)
