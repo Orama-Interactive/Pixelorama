42e8c8cb507e25a3c26469e31a745bbb9a557d54
Emmanouil Papadeas <manoschool@yahoo.gr>
2023-12-18 20:56:38 +0200

    When saving, don't include ".pxo" in the project name

--- src/Autoload/OpenSave.gd
+++ src/Autoload/OpenSave.gd
@@ -324,7 +324,7 @@ func save_pxo_file(
 	path: String, autosave: bool, include_blended := false, project := Global.current_project
 ) -> bool:
 	if !autosave:
-		project.name = path.get_file()
+		project.name = path.get_file().trim_suffix(".pxo")
 	var serialized_data := project.serialize()
 	if !serialized_data:
 		Global.error_dialog.set_text(
