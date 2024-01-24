5297fe6a80145dc3e9f50ec42734b541e95bdb39
Emmanouil Papadeas <manoschool@yahoo.gr>
2024-01-24 04:22:45 +0200

    Forgot to format the previous commit :(

--- src/Preferences/PreferencesDialog.gd
+++ src/Preferences/PreferencesDialog.gd
@@ -11,7 +11,9 @@ var preferences: Array[Preference] = [
 	Preference.new("shrink", "%ShrinkSlider", "value"),
 	Preference.new("font_size", "Interface/InterfaceOptions/FontSizeSlider", "value"),
 	Preference.new("dim_on_popup", "Interface/InterfaceOptions/DimCheckBox", "button_pressed"),
-	Preference.new("use_native_file_dialogs", "Interface/InterfaceOptions/NativeFileDialogs", "button_pressed"),
+	Preference.new(
+		"use_native_file_dialogs", "Interface/InterfaceOptions/NativeFileDialogs", "button_pressed"
+	),
 	Preference.new("icon_color_from", "Interface/ButtonOptions/IconColorOptionButton", "selected"),
 	Preference.new("custom_icon_color", "Interface/ButtonOptions/IconColorButton", "color"),
 	Preference.new("left_tool_color", "Interface/ButtonOptions/LeftToolColorButton", "color"),
