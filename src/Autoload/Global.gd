Orama-Interactive/Pixelorama

commit b08420d09d585446443f5fb91963b231d1f66b3a

Don't change the value of can_draw in Global.dialog_open()
In Godot 4 dialogs seem to be blocking the input to the rest of the UI, so this may not be needed

src/Autoload/Global.gd
@@ -805,11 +805,8 @@ func _renderer_changed(value: int) -> void:
 func dialog_open(open: bool) -> void:
 	var dim_color := Color.WHITE
 	if open:
-		can_draw = false
 		if dim_on_popup:
 			dim_color = Color(0.5, 0.5, 0.5)
-	else:
-		can_draw = true
 
 	var tween := create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT)
 	tween.tween_property(control, "modulate", dim_color, 0.1)

commit a7cf93d1e533f64720149a30cf413e1bed3cba67

Fix not being able to draw when closing the splash screen
Regression from b08420d0

src/Autoload/Global.gd
@@ -114,7 +114,7 @@ var current_project_index := 0:
 ## Tells if the user allowed to draw on the canvas. Usually it is temporarily set to
 ## [code]false[/code] when we are moving some gizmo and don't want the current tool to accidentally
 ## start drawing.[br](This does not depend on layer invisibility or lock/unlock status).
-var can_draw := false
+var can_draw := true
 ## (Intended to be used as getter only) Tells if the user allowed to move the guide while on canvas.
 var move_guides_on_canvas := true
 

src/Main.gd
@@ -153,8 +153,6 @@ func _show_splash_screen() -> void:
 
 		$Dialogs/SplashDialog.popup_centered()  # Splash screen
 		modulate = Color(0.5, 0.5, 0.5)
-	else:
-		Global.can_draw = true
 
 
 func _handle_backup() -> void:
@@ -176,7 +174,6 @@ func _handle_backup() -> void:
 				_on_BackupConfirmation_custom_action.bind(project_paths, backup_paths)
 			)
 			backup_confirmation.popup_centered()
-			Global.can_draw = false
 			modulate = Color(0.5, 0.5, 0.5)
 		else:
 			if Global.open_last_project: