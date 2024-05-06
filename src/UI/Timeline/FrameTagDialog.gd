02f85ade061613162cfd590de7da8f548a5511dc
Emmanouil Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2024-05-06 20:13:09 +0300

    Make TagOptions its own scene

--- src/UI/Timeline/FrameTagDialog.gd
+++ src/UI/Timeline/FrameTagDialog.gd
@@ -1,21 +1,10 @@
 extends AcceptDialog
 
-var current_tag_id := 0
 var tag_vboxes := []
-var delete_tag_button: Button
 
 @onready var main_vbox_cont: VBoxContainer = $VBoxContainer/ScrollContainer/VBoxTagContainer
 @onready var add_tag_button: Button = $VBoxContainer/ScrollContainer/VBoxTagContainer/AddTag
-@onready var options_dialog := $TagOptions
-@onready var name_line_edit := $TagOptions/GridContainer/NameLineEdit as LineEdit
-@onready var color_picker_button := $TagOptions/GridContainer/ColorPickerButton as ColorPickerButton
-@onready var from_spinbox := $TagOptions/GridContainer/FromSpinBox as SpinBox
-@onready var to_spinbox := $TagOptions/GridContainer/ToSpinBox as SpinBox
-@onready var user_data_text_edit := $TagOptions/GridContainer/UserDataTextEdit as TextEdit
-
-
-func _ready() -> void:
-	$"TagOptions/GridContainer/ColorPickerButton".get_picker().presets_visible = false
+@onready var options_dialog := $TagOptions as ConfirmationDialog
 
 
 func _on_FrameTagDialog_about_to_show() -> void:
@@ -68,111 +57,25 @@ func _on_FrameTagDialog_visibility_changed() -> void:
 func _on_AddTag_pressed() -> void:
 	var x_pos := add_tag_button.global_position.x
 	var y_pos := add_tag_button.global_position.y + 2 * add_tag_button.size.y
-	options_dialog.popup(Rect2i(position + Vector2i(x_pos, y_pos), options_dialog.size))
-	current_tag_id = Global.current_project.animation_tags.size()
+	var dialog_position := Rect2i(position + Vector2i(x_pos, y_pos), options_dialog.size)
+	var current_tag_id := Global.current_project.animation_tags.size()
 	# Determine tag values (array sort method)
-	var frames := []
+	var frames := PackedInt32Array([])
 	for cel in Global.current_project.selected_cels:
 		frames.append(cel[0])
 	frames.sort()
-
-	from_spinbox.value = (frames[0] + 1)
-	to_spinbox.value = (frames[-1] + 1)
-	color_picker_button.color = Color(randf(), randf(), randf())
-	user_data_text_edit.text = ""
+	options_dialog.show_dialog(dialog_position, current_tag_id, false, frames)
 
 
 func _on_EditButton_pressed(_tag_id: int, edit_button: Button) -> void:
 	var x_pos := edit_button.global_position.x
 	var y_pos := edit_button.global_position.y + 2 * edit_button.size.y
-	options_dialog.popup(Rect2i(position + Vector2i(x_pos, y_pos), options_dialog.size))
-	current_tag_id = _tag_id
-	var animation_tag := Global.current_project.animation_tags[_tag_id]
-	name_line_edit.text = animation_tag.name
-	color_picker_button.color = animation_tag.color
-	from_spinbox.value = animation_tag.from
-	to_spinbox.value = animation_tag.to
-	user_data_text_edit.text = animation_tag.user_data
-	if !delete_tag_button:
-		delete_tag_button = options_dialog.add_button("Delete", true, "delete_tag")
-	else:
-		delete_tag_button.visible = true
+	var dialog_position := Rect2i(position + Vector2i(x_pos, y_pos), options_dialog.size)
+	options_dialog.show_dialog(dialog_position, _tag_id, true)
 
 
-func _on_TagOptions_confirmed() -> void:
-	var tag_name := name_line_edit.text
-	var tag_color := color_picker_button.color
-	var tag_from := from_spinbox.value
-	var tag_to := to_spinbox.value
-	var user_data := user_data_text_edit.text
-
-	if tag_to > Global.current_project.frames.size():
-		tag_to = Global.current_project.frames.size()
-
-	if tag_from > tag_to:
-		tag_from = tag_to
-
-	var new_animation_tags := Global.current_project.animation_tags.duplicate()
-	# Loop through the tags to create new classes for them, so that they won't be the same
-	# as Global.current_project.animation_tags's classes. Needed for undo/redo to work properly.
-	for i in new_animation_tags.size():
-		var prev_tag: AnimationTag = new_animation_tags[i]
-		new_animation_tags[i] = AnimationTag.new(
-			prev_tag.name, prev_tag.color, prev_tag.from, prev_tag.to
-		)
-		new_animation_tags[i].user_data = prev_tag.user_data
-
-	if current_tag_id == Global.current_project.animation_tags.size():
-		var new_tag := AnimationTag.new(tag_name, tag_color, tag_from, tag_to)
-		new_tag.user_data = user_data
-		new_animation_tags.append(new_tag)
-	else:
-		new_animation_tags[current_tag_id].name = tag_name
-		new_animation_tags[current_tag_id].color = tag_color
-		new_animation_tags[current_tag_id].from = tag_from
-		new_animation_tags[current_tag_id].to = tag_to
-		new_animation_tags[current_tag_id].user_data = user_data
-
-	# Handle Undo/Redo
-	Global.current_project.undos += 1
-	Global.current_project.undo_redo.create_action("Modify Frame Tag")
-	Global.current_project.undo_redo.add_do_method(Global.general_redo)
-	Global.current_project.undo_redo.add_undo_method(Global.general_undo)
-	Global.current_project.undo_redo.add_do_property(
-		Global.current_project, "animation_tags", new_animation_tags
-	)
-	Global.current_project.undo_redo.add_undo_property(
-		Global.current_project, "animation_tags", Global.current_project.animation_tags
-	)
-	Global.current_project.undo_redo.commit_action()
-	_on_FrameTagDialog_about_to_show()
-
-
-func _on_TagOptions_custom_action(action: String) -> void:
-	if action != "delete_tag":
-		return
-	var new_animation_tags := Global.current_project.animation_tags.duplicate()
-	new_animation_tags.remove_at(current_tag_id)
-	# Handle Undo/Redo
-	Global.current_project.undos += 1
-	Global.current_project.undo_redo.create_action("Delete Frame Tag")
-	Global.current_project.undo_redo.add_do_method(Global.general_redo)
-	Global.current_project.undo_redo.add_undo_method(Global.general_undo)
-	Global.current_project.undo_redo.add_do_property(
-		Global.current_project, "animation_tags", new_animation_tags
-	)
-	Global.current_project.undo_redo.add_undo_property(
-		Global.current_project, "animation_tags", Global.current_project.animation_tags
-	)
-	Global.current_project.undo_redo.commit_action()
-
-	options_dialog.hide()
-	_on_FrameTagDialog_about_to_show()
-
-
-func _on_TagOptions_visibility_changed() -> void:
-	if delete_tag_button:
-		delete_tag_button.visible = false
+func _on_tag_options_visibility_changed() -> void:
+	_on_FrameTagDialog_about_to_show.call_deferred()
 
 
 func _on_PlayOnlyTags_toggled(button_pressed: bool) -> void:
---
+++ src/UI/Timeline/TagOptions.gd
@@ -0,0 +1,103 @@
+extends ConfirmationDialog
+
+var current_tag_id := 0
+@onready var delete_tag_button := add_button("Delete", true, "delete_tag")
+@onready var name_line_edit := $GridContainer/NameLineEdit as LineEdit
+@onready var color_picker_button := $GridContainer/ColorPickerButton as ColorPickerButton
+@onready var from_spinbox := $GridContainer/FromSpinBox as SpinBox
+@onready var to_spinbox := $GridContainer/ToSpinBox as SpinBox
+@onready var user_data_text_edit := $GridContainer/UserDataTextEdit as TextEdit
+
+
+func _ready() -> void:
+	color_picker_button.get_picker().presets_visible = false
+
+
+func show_dialog(
+	popup_rect: Rect2i, tag_id: int, is_editing: bool, selected_frames := PackedInt32Array()
+) -> void:
+	current_tag_id = tag_id
+	if is_editing:
+		var animation_tag := Global.current_project.animation_tags[tag_id]
+		name_line_edit.text = animation_tag.name
+		color_picker_button.color = animation_tag.color
+		from_spinbox.value = animation_tag.from
+		to_spinbox.value = animation_tag.to
+		user_data_text_edit.text = animation_tag.user_data
+		delete_tag_button.visible = true
+	else:
+		from_spinbox.value = (selected_frames[0] + 1)
+		to_spinbox.value = (selected_frames[-1] + 1)
+		color_picker_button.color = Color(randf(), randf(), randf())
+		user_data_text_edit.text = ""
+		delete_tag_button.visible = false
+	popup(popup_rect)
+
+
+func _on_confirmed() -> void:
+	var tag_name := name_line_edit.text
+	var tag_color := color_picker_button.color
+	var tag_from := from_spinbox.value
+	var tag_to := to_spinbox.value
+	var user_data := user_data_text_edit.text
+
+	if tag_to > Global.current_project.frames.size():
+		tag_to = Global.current_project.frames.size()
+
+	if tag_from > tag_to:
+		tag_from = tag_to
+
+	var new_animation_tags := Global.current_project.animation_tags.duplicate()
+	# Loop through the tags to create new classes for them, so that they won't be the same
+	# as Global.current_project.animation_tags's classes. Needed for undo/redo to work properly.
+	for i in new_animation_tags.size():
+		var prev_tag: AnimationTag = new_animation_tags[i]
+		new_animation_tags[i] = AnimationTag.new(
+			prev_tag.name, prev_tag.color, prev_tag.from, prev_tag.to
+		)
+		new_animation_tags[i].user_data = prev_tag.user_data
+
+	if current_tag_id == Global.current_project.animation_tags.size():
+		var new_tag := AnimationTag.new(tag_name, tag_color, tag_from, tag_to)
+		new_tag.user_data = user_data
+		new_animation_tags.append(new_tag)
+	else:
+		new_animation_tags[current_tag_id].name = tag_name
+		new_animation_tags[current_tag_id].color = tag_color
+		new_animation_tags[current_tag_id].from = tag_from
+		new_animation_tags[current_tag_id].to = tag_to
+		new_animation_tags[current_tag_id].user_data = user_data
+
+	# Handle Undo/Redo
+	Global.current_project.undos += 1
+	Global.current_project.undo_redo.create_action("Modify Frame Tag")
+	Global.current_project.undo_redo.add_do_method(Global.general_redo)
+	Global.current_project.undo_redo.add_undo_method(Global.general_undo)
+	Global.current_project.undo_redo.add_do_property(
+		Global.current_project, "animation_tags", new_animation_tags
+	)
+	Global.current_project.undo_redo.add_undo_property(
+		Global.current_project, "animation_tags", Global.current_project.animation_tags
+	)
+	Global.current_project.undo_redo.commit_action()
+
+
+func _on_custom_action(action: String) -> void:
+	if action != "delete_tag":
+		return
+	var new_animation_tags := Global.current_project.animation_tags.duplicate()
+	new_animation_tags.remove_at(current_tag_id)
+	# Handle Undo/Redo
+	Global.current_project.undos += 1
+	Global.current_project.undo_redo.create_action("Delete Frame Tag")
+	Global.current_project.undo_redo.add_do_method(Global.general_redo)
+	Global.current_project.undo_redo.add_undo_method(Global.general_undo)
+	Global.current_project.undo_redo.add_do_property(
+		Global.current_project, "animation_tags", new_animation_tags
+	)
+	Global.current_project.undo_redo.add_undo_property(
+		Global.current_project, "animation_tags", Global.current_project.animation_tags
+	)
+	Global.current_project.undo_redo.commit_action()
+
+	hide()
