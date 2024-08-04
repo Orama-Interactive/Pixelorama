fbe29523464b8792a1e3172fef88980344bd77fc
Emmanouil Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2024-08-04 22:12:57 +0300

    Make shape previews look like they did in v1.0
    The AutoInvertColors shader has been updated to make shapes hollow, similar to how the marching ants outline works.

--- src/UI/Canvas/Previews.gd
+++ src/UI/Canvas/Previews.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 
+func _ready() -> void:
+	Global.camera.zoom_changed.connect(_update_on_zoom)
+
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouse or event is InputEventKey:
 		queue_redraw()
@@ -9,3 +13,7 @@ func _input(event: InputEvent) -> void:
 func _draw() -> void:
 	if Global.can_draw:
 		Tools.draw_preview()
+
+
+func _update_on_zoom() -> void:
+	material.set_shader_parameter("width", 1.0 / Global.camera.zoom.x)
