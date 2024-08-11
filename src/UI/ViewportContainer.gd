eca55c2c9d8314afb1719c523545be2b243a5ee5
Hamster5295 <hamster5295@163.com>
2024-08-11 20:41:53 +0800

    format: reformat according to gdlint

--- src/UI/ViewportContainer.gd
+++ src/UI/ViewportContainer.gd
@@ -2,9 +2,9 @@ extends SubViewportContainer
 
 @export var camera_path: NodePath
 
-@onready var camera := get_node(camera_path) as CanvasCamera
+var _mouse_inside = false
 
-var mouse_inside = false
+@onready var camera := get_node(camera_path) as CanvasCamera
 
 
 func _ready() -> void:
@@ -36,16 +36,16 @@ func _ready() -> void:
 func _input(event):
 	if event is InputEventMouseMotion:
 		if get_global_rect().has_point(event.position):
-			if !mouse_inside:
-				mouse_inside = true
+			if !_mouse_inside:
+				_mouse_inside = true
 				camera.set_process_input(true)
 				Global.control.left_cursor.visible = Global.show_left_tool_icon
 				Global.control.right_cursor.visible = Global.show_right_tool_icon
 				if Global.cross_cursor:
 					Input.set_default_cursor_shape(Input.CURSOR_CROSS)
 		else:
-			if mouse_inside:
-				mouse_inside = false
+			if _mouse_inside:
+				_mouse_inside = false
 				camera.drag = false
 				Global.control.left_cursor.visible = false
 				Global.control.right_cursor.visible = false
