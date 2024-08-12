f56a29aa2378b3f9aea167b8075df4b8e3966ed3
Hamster5295 <hamster5295@163.com>
2024-08-13 00:06:34 +0800

    refactor: removed unused code comments

--- src/UI/ViewportContainer.gd
+++ src/UI/ViewportContainer.gd
@@ -12,27 +12,6 @@ func _ready() -> void:
 	material.blend_mode = CanvasItemMaterial.BLEND_MODE_PREMULT_ALPHA
 
 
-#func _on_ViewportContainer_mouse_entered() -> void:
-#return
-#camera.set_process_input(true)
-#Global.control.left_cursor.visible = Global.show_left_tool_icon
-#Global.control.right_cursor.visible = Global.show_right_tool_icon
-#if Global.cross_cursor:
-#Input.set_default_cursor_shape(Input.CURSOR_CROSS)
-#print("set cross")
-#
-#
-#func _on_ViewportContainer_mouse_exited() -> void:
-#return
-#print("exit")
-#camera.set_process_input(false)
-#camera.drag = false
-#Global.control.left_cursor.visible = false
-#Global.control.right_cursor.visible = false
-#Input.set_default_cursor_shape(Input.CURSOR_ARROW)
-#print("set arrow")
-
-
 func _input(event):
 	if event is InputEventMouseMotion:
 		if get_global_rect().has_point(event.position):
