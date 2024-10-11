837204f84398a61f796c2a130a456f87c1a42c04
Variable <pkfayezakhtar@gmail.com>
2024-10-11 19:20:12 +0500

    somewhat fix transparency

--- src/UI/UI.gd
+++ src/UI/UI.gd
@@ -2,9 +2,25 @@ extends Panel
 
 @onready var main_canvas_container := find_child("Main Canvas") as Container
 
+var shader_moved := false
+var transparency_material: ShaderMaterial
 
 func _ready() -> void:
+	transparency_material = material
+	main_canvas_container.property_list_changed.connect(_re_configure_shader)
 	update_transparent_shader()
+
+
+func _re_configure_shader():
+	await get_tree().process_frame
+	if get_window() != main_canvas_container.get_window():
+		main_canvas_container.material = transparency_material
+		material = null
+		shader_moved = true
+	else:
+		if shader_moved:
+			material = transparency_material
+			shader_moved = false
 
 
 func _on_main_canvas_item_rect_changed() -> void:
@@ -20,6 +36,6 @@ func update_transparent_shader() -> void:
 		return
 	# Works independently of the transparency feature
 	var canvas_size: Vector2 = (main_canvas_container.size - Vector2.DOWN * 2) * Global.shrink
-	material.set_shader_parameter("screen_resolution", get_viewport().size)
-	material.set_shader_parameter("position", main_canvas_container.global_position * Global.shrink)
-	material.set_shader_parameter("size", canvas_size)
+	transparency_material.set_shader_parameter("screen_resolution", get_viewport().size)
+	transparency_material.set_shader_parameter("position", main_canvas_container.global_position * Global.shrink)
+	transparency_material.set_shader_parameter("size", canvas_size)
