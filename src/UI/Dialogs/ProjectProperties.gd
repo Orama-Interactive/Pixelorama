7f933d9cb76f72a37374ed6419ba7dacbf349e23
Emmanouil Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2024-11-20 03:09:37 +0200

    Show the color mode in the project properties dialog

--- src/UI/Dialogs/ProjectProperties.gd
+++ src/UI/Dialogs/ProjectProperties.gd
@@ -1,6 +1,7 @@
 extends AcceptDialog
 
 @onready var size_value_label := $GridContainer/SizeValueLabel as Label
+@onready var color_mode_value_label := $GridContainer/ColorModeValueLabel as Label
 @onready var frames_value_label := $GridContainer/FramesValueLabel as Label
 @onready var layers_value_label := $GridContainer/LayersValueLabel as Label
 @onready var name_line_edit := $GridContainer/NameLineEdit as LineEdit
@@ -10,6 +11,12 @@ extends AcceptDialog
 func _on_visibility_changed() -> void:
 	Global.dialog_open(visible)
 	size_value_label.text = str(Global.current_project.size)
+	if Global.current_project.get_image_format() == Image.FORMAT_RGBA8:
+		color_mode_value_label.text = "RGBA8"
+	else:
+		color_mode_value_label.text = str(Global.current_project.get_image_format())
+	if Global.current_project.is_indexed():
+		color_mode_value_label.text += " (%s)" % tr("Indexed")
 	frames_value_label.text = str(Global.current_project.frames.size())
 	layers_value_label.text = str(Global.current_project.layers.size())
 	name_line_edit.text = Global.current_project.name
