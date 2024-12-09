Orama-Interactive/Pixelorama

commit f91bb18fb2268b1257a664a52daf2a77e8051f6d

Implement tilemap layers (#1146)

src/Classes/Project.gd
@@ -85,6 +85,7 @@ var selection_offset := Vector2i.ZERO:
 		selection_offset = value
 		Global.canvas.selection.marching_ants_outline.offset = selection_offset
 var has_selection := false
+var tilesets: Array[TileSetCustom]
 
 ## For every camera (currently there are 3)
 var cameras_rotation: PackedFloat32Array = [0.0, 0.0, 0.0]
@@ -295,6 +296,9 @@ func serialize() -> Dictionary:
 	var reference_image_data := []
 	for reference_image in reference_images:
 		reference_image_data.append(reference_image.serialize())
+	var tileset_data := []
+	for tileset in tilesets:
+		tileset_data.append(tileset.serialize())
 
 	var metadata := _serialize_metadata(self)
 
@@ -315,6 +319,7 @@ func serialize() -> Dictionary:
 		"frames": frame_data,
 		"brushes": brush_data,
 		"reference_images": reference_image_data,
+		"tilesets": tileset_data,
 		"vanishing_points": vanishing_points,
 		"export_file_name": file_name,
 		"export_file_format": file_format,
@@ -344,6 +349,12 @@ func deserialize(dict: Dictionary, zip_reader: ZIPReader = null, file: FileAcces
 	if dict.has("tile_mode_y_basis_x") and dict.has("tile_mode_y_basis_y"):
 		tiles.y_basis.x = dict.tile_mode_y_basis_x
 		tiles.y_basis.y = dict.tile_mode_y_basis_y
+	if dict.has("tilesets"):
+		for saved_tileset in dict["tilesets"]:
+			var tile_size = str_to_var("Vector2i" + saved_tileset.get("tile_size"))
+			var tileset := TileSetCustom.new(tile_size)
+			tileset.deserialize(saved_tileset)
+			tilesets.append(tileset)
 	if dict.has("frames") and dict.has("layers"):
 		for saved_layer in dict.layers:
 			match int(saved_layer.get("type", Global.LayerTypes.PIXEL)):
@@ -353,63 +364,8 @@ func deserialize(dict: Dictionary, zip_reader: ZIPReader = null, file: FileAcces
 					layers.append(GroupLayer.new(self))
 				Global.LayerTypes.THREE_D:
 					layers.append(Layer3D.new(self))
-
-		var frame_i := 0
-		for frame in dict.frames:
-			var cels: Array[BaseCel] = []
-			var cel_i := 0
-			for cel in frame.cels:
-				match int(dict.layers[cel_i].get("type", Global.LayerTypes.PIXEL)):
-					Global.LayerTypes.PIXEL:
-						var image: Image
-						var indices_data := PackedByteArray()
-						if is_instance_valid(zip_reader):  # For pxo files saved in 1.0+
-							var path := "image_data/frames/%s/layer_%s" % [frame_i + 1, cel_i + 1]
-							var image_data := zip_reader.read_file(path)
-							image = Image.create_from_data(
-								size.x, size.y, false, get_image_format(), image_data
-							)
-							var indices_path := (
-								"image_data/frames/%s/indices_layer_%s" % [frame_i + 1, cel_i + 1]
-							)
-							if zip_reader.file_exists(indices_path):
-								indices_data = zip_reader.read_file(indices_path)
-						elif is_instance_valid(file):  # For pxo files saved in 0.x
-							var buffer := file.get_buffer(size.x * size.y * 4)
-							image = Image.create_from_data(
-								size.x, size.y, false, get_image_format(), buffer
-							)
-						var pixelorama_image := ImageExtended.new()
-						pixelorama_image.is_indexed = is_indexed()
-						if not indices_data.is_empty() and is_indexed():
-							pixelorama_image.indices_image = Image.create_from_data(
-								size.x, size.y, false, Image.FORMAT_R8, indices_data
-							)
-						pixelorama_image.copy_from(image)
-						pixelorama_image.select_palette("", true)
-						cels.append(PixelCel.new(pixelorama_image))
-					Global.LayerTypes.GROUP:
-						cels.append(GroupCel.new())
-					Global.LayerTypes.THREE_D:
-						if is_instance_valid(file):  # For pxo files saved in 0.x
-							# Don't do anything with it, just read it so that the file can move on
-							file.get_buffer(size.x * size.y * 4)
-						cels.append(Cel3D.new(size, true))
-				cel["pxo_version"] = pxo_version
-				cels[cel_i].deserialize(cel)
-				_deserialize_metadata(cels[cel_i], cel)
-				cel_i += 1
-			var duration := 1.0
-			if frame.has("duration"):
-				duration = frame.duration
-			elif dict.has("frame_duration"):
-				duration = dict.frame_duration[frame_i]
-
-			var frame_class := Frame.new(cels, duration)
-			frame_class.user_data = frame.get("user_data", "")
-			_deserialize_metadata(frame_class, frame)
-			frames.append(frame_class)
-			frame_i += 1
+				Global.LayerTypes.TILEMAP:
+					layers.append(LayerTileMap.new(self, null))
 
 		# Parent references to other layers are created when deserializing
 		# a layer, so loop again after creating them:
@@ -425,6 +381,43 @@ func deserialize(dict: Dictionary, zip_reader: ZIPReader = null, file: FileAcces
 				layer_dict["blend_mode"] = blend_mode
 			layers[layer_i].deserialize(layer_dict)
 			_deserialize_metadata(layers[layer_i], dict.layers[layer_i])
+
+		var frame_i := 0
+		for frame in dict.frames:
+			var cels: Array[BaseCel] = []
+			var cel_i := 0
+			for cel in frame.cels:
+				var layer := layers[cel_i]
+				match layer.get_layer_type():
+					Global.LayerTypes.PIXEL:
+						var image := _load_image_from_pxo(frame_i, cel_i, zip_reader, file)
+						cels.append(PixelCel.new(image))
+					Global.LayerTypes.GROUP:
+						cels.append(GroupCel.new())
+					Global.LayerTypes.THREE_D:
+						if is_instance_valid(file):  # For pxo files saved in 0.x
+							# Don't do anything with it, just read it so that the file can move on
+							file.get_buffer(size.x * size.y * 4)
+						cels.append(Cel3D.new(size, true))
+					Global.LayerTypes.TILEMAP:
+						var image := _load_image_from_pxo(frame_i, cel_i, zip_reader, file)
+						var new_cel := (layer as LayerTileMap).new_cel_from_image(image)
+						cels.append(new_cel)
+				cel["pxo_version"] = pxo_version
+				cels[cel_i].deserialize(cel)
+				_deserialize_metadata(cels[cel_i], cel)
+				cel_i += 1
+			var duration := 1.0
+			if frame.has("duration"):
+				duration = frame.duration
+			elif dict.has("frame_duration"):
+				duration = dict.frame_duration[frame_i]
+
+			var frame_class := Frame.new(cels, duration)
+			frame_class.user_data = frame.get("user_data", "")
+			_deserialize_metadata(frame_class, frame)
+			frames.append(frame_class)
+			frame_i += 1
 	if dict.has("tags"):
 		for tag in dict.tags:
 			var new_tag := AnimationTag.new(tag.name, Color(tag.color), tag.from, tag.to)
@@ -481,6 +474,37 @@ func _deserialize_metadata(object: Object, dict: Dictionary) -> void:
 	var metadata: Dictionary = dict["metadata"]
 	for meta in metadata.keys():
 		object.set_meta(meta, metadata[meta])
+
+
+## Called by [method deserialize], this method loads an image at
+## a given [param frame_i] frame index and a [param cel_i] cel index from a pxo file,
+## and returns it as an [ImageExtended].
+## If the pxo file is saved with Pixelorama version 1.0 and on,
+## the [param zip_reader] is used to load the image. Otherwise, [param file] is used.
+func _load_image_from_pxo(
+	frame_i: int, cel_i: int, zip_reader: ZIPReader, file: FileAccess
+) -> ImageExtended:
+	var image: Image
+	var indices_data := PackedByteArray()
+	if is_instance_valid(zip_reader):  # For pxo files saved in 1.0+
+		var path := "image_data/frames/%s/layer_%s" % [frame_i + 1, cel_i + 1]
+		var image_data := zip_reader.read_file(path)
+		image = Image.create_from_data(size.x, size.y, false, get_image_format(), image_data)
+		var indices_path := "image_data/frames/%s/indices_layer_%s" % [frame_i + 1, cel_i + 1]
+		if zip_reader.file_exists(indices_path):
+			indices_data = zip_reader.read_file(indices_path)
+	elif is_instance_valid(file):  # For pxo files saved in 0.x
+		var buffer := file.get_buffer(size.x * size.y * 4)
+		image = Image.create_from_data(size.x, size.y, false, get_image_format(), buffer)
+	var pixelorama_image := ImageExtended.new()
+	pixelorama_image.is_indexed = is_indexed()
+	if not indices_data.is_empty() and is_indexed():
+		pixelorama_image.indices_image = Image.create_from_data(
+			size.x, size.y, false, Image.FORMAT_R8, indices_data
+		)
+	pixelorama_image.copy_from(image)
+	pixelorama_image.select_palette("", true)
+	return pixelorama_image
 
 
 func _size_changed(value: Vector2i) -> void:
@@ -630,6 +654,57 @@ func get_all_pixel_cels() -> Array[PixelCel]:
 			if cel is PixelCel:
 				cels.append(cel)
 	return cels
+
+
+## Reads data from [param cels] and appends them to [param data],
+## to be used for the undo/redo system.
+## It adds data such as the images of [PixelCel]s,
+## and calls [method CelTileMap.serialize_undo_data] for [CelTileMap]s.
+func serialize_cel_undo_data(cels: Array[BaseCel], data: Dictionary) -> void:
+	var cels_to_serialize := cels
+	if not TileSetPanel.placing_tiles:
+		cels_to_serialize = find_same_tileset_tilemap_cels(cels)
+	for cel in cels_to_serialize:
+		if not cel is PixelCel:
+			continue
+		var image := (cel as PixelCel).get_image()
+		image.add_data_to_dictionary(data)
+		if cel is CelTileMap:
+			data[cel] = (cel as CelTileMap).serialize_undo_data()
+
+
+## Loads data from [param redo_data] and param [undo_data],
+## to be used for the undo/redo system.
+## It calls [method Global.undo_redo_compress_images], and
+## [method CelTileMap.deserialize_undo_data] for [CelTileMap]s.
+func deserialize_cel_undo_data(redo_data: Dictionary, undo_data: Dictionary) -> void:
+	Global.undo_redo_compress_images(redo_data, undo_data, self)
+	for cel in redo_data:
+		if cel is CelTileMap:
+			(cel as CelTileMap).deserialize_undo_data(redo_data[cel], undo_redo, false)
+	for cel in undo_data:
+		if cel is CelTileMap:
+			(cel as CelTileMap).deserialize_undo_data(undo_data[cel], undo_redo, true)
+
+
+## Returns all [BaseCel]s in [param cels], and for every [CelTileMap],
+## this methods finds all other [CelTileMap]s that share the same [TileSetCustom],
+## and appends them in the array that is being returned by this method.
+func find_same_tileset_tilemap_cels(cels: Array[BaseCel]) -> Array[BaseCel]:
+	var tilemap_cels: Array[BaseCel]
+	var current_tilesets: Array[TileSetCustom]
+	for cel in cels:
+		tilemap_cels.append(cel)
+		if cel is not CelTileMap:
+			continue
+		current_tilesets.append((cel as CelTileMap).tileset)
+	for cel in get_all_pixel_cels():
+		if cel is not CelTileMap:
+			continue
+		if (cel as CelTileMap).tileset in current_tilesets:
+			if cel not in cels:
+				tilemap_cels.append(cel)
+	return tilemap_cels
 
 
 ## Re-order layers to take each cel's z-index into account. If all z-indexes are 0,
@@ -931,3 +1006,16 @@ func reorder_reference_image(from: int, to: int) -> void:
 	var ri: ReferenceImage = reference_images.pop_at(from)
 	reference_images.insert(to, ri)
 	Global.canvas.reference_image_container.move_child(ri, to)
+
+
+## Adds a new [param tileset] to [member tilesets].
+func add_tileset(tileset: TileSetCustom) -> void:
+	tilesets.append(tileset)
+
+
+## Loops through all cels in [param cel_dictionary], and for [CelTileMap]s,
+## it calls [method CelTileMap.update_tilemap].
+func update_tilemaps(cel_dictionary: Dictionary) -> void:
+	for cel in cel_dictionary:
+		if cel is CelTileMap:
+			(cel as CelTileMap).update_tilemap()

commit 11e05ac4717654bad4fc3bcacc1d72afbe303984

Fix loading project with linked cels
Regression from #1146.

src/Classes/Project.gd
@@ -367,21 +367,6 @@ func deserialize(dict: Dictionary, zip_reader: ZIPReader = null, file: FileAcces
 				Global.LayerTypes.TILEMAP:
 					layers.append(LayerTileMap.new(self, null))
 
-		# Parent references to other layers are created when deserializing
-		# a layer, so loop again after creating them:
-		for layer_i in dict.layers.size():
-			layers[layer_i].index = layer_i
-			var layer_dict: Dictionary = dict.layers[layer_i]
-			# Ensure that loaded pxo files from v1.0-v1.0.3 have the correct
-			# blend mode, after the addition of the Erase mode in v1.0.4.
-			if pxo_version < 4 and layer_dict.has("blend_mode"):
-				var blend_mode: int = layer_dict.get("blend_mode")
-				if blend_mode >= BaseLayer.BlendModes.ERASE:
-					blend_mode += 1
-				layer_dict["blend_mode"] = blend_mode
-			layers[layer_i].deserialize(layer_dict)
-			_deserialize_metadata(layers[layer_i], dict.layers[layer_i])
-
 		var frame_i := 0
 		for frame in dict.frames:
 			var cels: Array[BaseCel] = []
@@ -401,7 +386,9 @@ func deserialize(dict: Dictionary, zip_reader: ZIPReader = null, file: FileAcces
 						cels.append(Cel3D.new(size, true))
 					Global.LayerTypes.TILEMAP:
 						var image := _load_image_from_pxo(frame_i, cel_i, zip_reader, file)
-						var new_cel := (layer as LayerTileMap).new_cel_from_image(image)
+						var tileset_index = dict.layers[cel_i].tileset_index
+						var tileset := tilesets[tileset_index]
+						var new_cel := CelTileMap.new(tileset, image)
 						cels.append(new_cel)
 				cel["pxo_version"] = pxo_version
 				cels[cel_i].deserialize(cel)
@@ -418,6 +405,21 @@ func deserialize(dict: Dictionary, zip_reader: ZIPReader = null, file: FileAcces
 			_deserialize_metadata(frame_class, frame)
 			frames.append(frame_class)
 			frame_i += 1
+
+		# Parent references to other layers are created when deserializing
+		# a layer, so loop again after creating them:
+		for layer_i in dict.layers.size():
+			layers[layer_i].index = layer_i
+			var layer_dict: Dictionary = dict.layers[layer_i]
+			# Ensure that loaded pxo files from v1.0-v1.0.3 have the correct
+			# blend mode, after the addition of the Erase mode in v1.0.4.
+			if pxo_version < 4 and layer_dict.has("blend_mode"):
+				var blend_mode: int = layer_dict.get("blend_mode")
+				if blend_mode >= BaseLayer.BlendModes.ERASE:
+					blend_mode += 1
+				layer_dict["blend_mode"] = blend_mode
+			layers[layer_i].deserialize(layer_dict)
+			_deserialize_metadata(layers[layer_i], dict.layers[layer_i])
 	if dict.has("tags"):
 		for tag in dict.tags:
 			var new_tag := AnimationTag.new(tag.name, Color(tag.color), tag.from, tag.to)