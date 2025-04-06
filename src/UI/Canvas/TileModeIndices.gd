73bed6f51f0613b2f2d634a8da17ad0df451161f
Emmanouil Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2025-04-06 22:48:26 +0300

    Scale indices based on cell size

--- src/UI/Canvas/TileModeIndices.gd
+++ src/UI/Canvas/TileModeIndices.gd
@@ -10,11 +10,12 @@ func _input(event: InputEvent) -> void:
 
 func _draw() -> void:
 	var current_cel := Global.current_project.get_current_cel()
-	var scale_factor := 0.25
-	draw_set_transform(position, rotation, Vector2(scale_factor, scale_factor))
 	if current_cel is CelTileMap and Input.is_action_pressed("ctrl"):
 		var tilemap_cel := current_cel as CelTileMap
 		var tile_size := tilemap_cel.get_tile_size()
+		var min_axis := mini(tilemap_cel.get_tile_size().x, tilemap_cel.get_tile_size().y)
+		var scale_factor := min_axis / 32.0
+		draw_set_transform(position, rotation, Vector2(scale_factor, scale_factor))
 		var font := Themes.get_font()
 		for cell_coords: Vector2i in tilemap_cel.cells:
 			var cell := tilemap_cel.get_cell_at(cell_coords)
@@ -22,7 +23,7 @@ func _draw() -> void:
 				continue
 			var text := cell.to_string()
 			var pos := tilemap_cel.get_pixel_coords(cell_coords)
-			pos.y += tile_size.y - font.get_ascent(FONT_SIZE * 0.5) * 0.5
+			pos.y += tile_size.y - font.get_ascent(FONT_SIZE * 0.5) * scale_factor
 			draw_string(
 				font,
 				pos / scale_factor,
