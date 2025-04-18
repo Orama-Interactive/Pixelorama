426445d3b84c4c9cf2b88281c646f9678d35fe67
Variable <77773850+Variable-ind@users.noreply.github.com>
2025-04-18 10:50:33 +0500

    fix some formatting and stuff

--- src/UI/Timeline/FrameScrollContainer.gd
+++ src/UI/Timeline/FrameScrollContainer.gd
@@ -16,8 +16,8 @@ func _gui_input(event: InputEvent) -> void:
 	if get_child_count():
 		var vertical_scroll: bool = get_child(0).size.y >= size.y
 		print("Event:", event)
-		print("Shift pressed:",event.shift_pressed)
-		var should_h_scroll = (event.shift_pressed or not vertical_scroll)
+		print("Shift pressed:", event.shift_pressed)
+		var should_h_scroll = event.shift_pressed or not vertical_scroll
 		if event is InputEventMouseButton:
 			if event.button_index in [MOUSE_BUTTON_WHEEL_RIGHT, MOUSE_BUTTON_WHEEL_LEFT]:
 				# This helps/allows two finger scrolling (on Laptops)
@@ -36,7 +36,7 @@ func _gui_input(event: InputEvent) -> void:
 					):
 						h_scroll_bar.value += Global.animation_timeline.cel_size / 2 + 2
 						accept_event()
-		elif event is InputEventPanGesture and (event.shift_pressed or not vertical_scroll):
+		elif event is InputEventPanGesture:
 			if event.delta.x != 0:
 				h_scroll_bar.value += (
 					sign(event.delta.x) * (Global.animation_timeline.cel_size / 2 + 2)
