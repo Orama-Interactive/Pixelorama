8c3181f440358c1b1542ffdf9756c878ae880c7b
Variable-ind <77773850+Variable-ind@users.noreply.github.com>
2025-07-31 00:24:56 +0500

    slightly working

--- src/Classes/Layers/BoneLayer.gd
+++ src/Classes/Layers/BoneLayer.gd
@@ -9,6 +9,7 @@ var enabled := true
 var ignore_rotation_hover := false
 var modify_mode := NONE
 var generation_cache: Dictionary
+var render_array: Array[ImageExtended]
 
 
 func _init(_project: Project, _name := "") -> void:
@@ -29,91 +30,6 @@ func get_parent_bone():
 			break
 		bone_parent = bone_parent.parent
 	return bone_parent
-
-
-func apply_bone(render: ImageExtended, cel_image: ImageExtended, at_frame: Frame) -> Image:
-	if name == "Group 2":
-		cel_image.save_png("out/a")
-	if not enabled:
-		return cel_image
-	var bone_cel: BoneCel = at_frame.cels[index]
-	var used_region := cel_image.get_used_rect()
-	var start_point: Vector2i = bone_cel.start_point
-	var gizmo_origin: Vector2i = bone_cel.gizmo_origin.floor()
-	var angle: float = bone_cel.bone_rotation
-	var bone_parent = get_parent_bone()
-	if bone_parent:
-		start_point -= Vector2i(at_frame.cels[bone_parent.index].start_point)
-		gizmo_origin -= Vector2i(at_frame.cels[bone_parent.index].gizmo_origin)
-	else:
-		angle = 0
-	if angle == 0 and start_point == Vector2i.ZERO:
-		return cel_image
-	if used_region.size == Vector2i.ZERO:
-		return cel_image
-	# Imprint on a square for rotation
-	# (We are doing this so that the image doesn't get clipped as a result of rotation.)
-	var diagonal_length := floori(used_region.size.length())
-	if diagonal_length % 2 == 0:
-		diagonal_length += 1
-	var s_offset: Vector2i = (
-		0.5 * (Vector2i(diagonal_length, diagonal_length)
-		- used_region.size)
-	).floor()
-	var square_image = cel_image.get_region(
-		Rect2i(used_region.position - s_offset, Vector2i(diagonal_length, diagonal_length))
-	)
-	# Apply Rotation To this Image
-	if angle != 0:
-		var transformation_matrix := Transform2D(angle, Vector2.ZERO)
-		var rotate_params := {
-			"transformation_matrix": transformation_matrix.affine_inverse(),
-			"pivot": Vector2(0.5, 0.5),
-			"ending_angle": angle,
-			"tolerance": 0,
-			"preview": false
-		}
-		# Detects if the rotation is changed for this generation or not
-		# (useful if bone is moved arround while having some rotation)
-		# NOTE: I tried cacheing entire poses (that remain same) as well. It was faster than this
-		# approach but only by a few milliseconds. I don't think straining the memory for only
-		# a boost of a few millisec was worth it so i declare this the most optimal approach.
-		var cache_key := {"angle": angle, "cel_content": cel_image.get_data()}
-		var bone_cache: Dictionary = generation_cache.get_or_add(bone_cel, {})
-		if cache_key in bone_cache.keys():
-			square_image = bone_cache[cache_key]
-		else:
-			var gen = ShaderImageEffect.new()
-			gen.generate_image(
-				square_image,
-				DrawingAlgos.nn_shader,
-				rotate_params, square_image.get_size()
-			)
-			bone_cache.clear()
-			bone_cache[cache_key] = square_image
-	if name == "Group 2":
-		square_image.save_png("out/test")
-		cel_image.save_png("out/cel %s" % name)
-	var pivot: Vector2i = gizmo_origin
-	var bone_start_global: Vector2i = gizmo_origin + start_point
-	var square_image_start: Vector2i = used_region.position - s_offset
-	var global_square_centre: Vector2 = square_image_start + (square_image.get_size() / 2)
-	var global_rotated_new_centre = (
-		(global_square_centre - Vector2(pivot)).rotated(angle)
-		+ Vector2(bone_start_global)
-	)
-	var new_start: Vector2i = (
-		square_image_start
-		+ Vector2i((global_rotated_new_centre - global_square_centre).floor())
-	)
-	if !render.is_empty():
-		render.fill(Color(0, 0, 0, 0))
-	render.blit_rect(
-		square_image,
-		Rect2i(Vector2.ZERO, square_image.get_size()),
-		Vector2i(new_start)
-	)
-	return render
 
 
 #func get_best_origin(layer_idx: int) -> Vector2i:
@@ -204,43 +120,179 @@ static func _is_close_to_segment(
 func blend_children(frame: Frame, origin := Vector2i.ZERO, apply_effects := true) -> Image:
 	if project.current_layer == index:
 		Global.canvas.skeleton.queue_redraw()
-	var image = super.blend_children(frame, origin, apply_effects)
-	if get_parent_bone() == null:
-		image = apply_bone(image, image, frame)
+	var image: ImageExtended = generate_pose(frame)
 	return image
 
-## Include a child group in the blending process.
-## If the child group is set to pass through mode, loop through its children
-## and include them as separate images, instead of blending them all together.
-## Gets called recursively if the child group has children groups of its own,
-## and they are also set to pass through mode.
-func _blend_child_group(
-	image: ImageExtended,
-	layer: BaseLayer,
-	frame: Frame,
-	textures: Array[Image],
-	metadata_image: Image,
-	i: int,
-	origin: Vector2i,
-	apply_effects: bool
-) -> int:
-	var new_i := i
-	var blend_rect := Rect2i(Vector2i.ZERO, project.size)
-	var cel := frame.cels[layer.index]
-	var blended_children := (layer as GroupLayer).blend_children(frame, origin)
-	if layer is BoneLayer:
-		var bone_render = ImageExtended.create_custom(
+
+func generate_pose(frame: Frame) -> ImageExtended:
+	# Do we even need to generate a pose?
+	var project = Global.current_project
+	var image = ImageExtended.create_custom(
 		project.size.x, project.size.y, false, project.get_image_format(), project.is_indexed()
-		)
-		blended_children = layer.apply_bone(bone_render, blended_children, frame)
-	if DisplayServer.get_name() == "headless":
-		image.blend_rect(blended_children, blend_rect, origin)
+	)
+	if not get_parent_bone() == null:  # We don't want to render to a child
+		return image
+	if not visible:  # Pose Layer is invisible (So generating is a waste of time)
+		return image
+
+	# Start generating
+	# (Group visibility is completely ignored while the visibility of other layer types is respected)
+	var previous_ordered_layers: Array[int] = project.ordered_layers
+	var for_frame = project.frames.find(frame)
+	project.order_layers(for_frame)
+	var textures: Array[Image] = []
+	# Nx4 texture, where N is the number of layers and the first row are the blend modes,
+	# the second are the opacities, the third are the origins and the fourth are the
+	# clipping mask booleans.
+	var metadata_image := Image.create_empty(project.layers.size(), 4, false, Image.FORMAT_R8)
+	for i in project.layers.size():
+		var ordered_index = project.ordered_layers[i]
+		var layer = project.layers[ordered_index]
+		var bone_layer = layer.parent
+		# Ignore visibility for group layers
+		if bone_layer:
+			if bone_layer != self and not self.is_ancestor_of(bone_layer):
+				continue
+		var cel = frame.cels[ordered_index]
+		var cel_image: Image
+		if layer == self or not is_instance_valid(bone_layer):
+			_set_layer_metadata_image(layer, cel, metadata_image, ordered_index, false)
+			continue
+
+		var include := true if layer.is_visible_in_hierarchy() else false
+		print(layer, include)
+		if layer.is_blender():
+			cel_image = layer.blend_children(frame)
+		else:
+			cel_image = layer.display_effects(cel)
+
+		#if bone_layer is BoneLayer and is_instance_valid(bone_layer):
+			#bone_layer.apply_bone(cel_image, frame)
+
+		textures.append(cel_image)
+		if (
+			layer.is_blended_by_ancestor()
+		):
+			include = false
+		_set_layer_metadata_image(layer, cel, metadata_image, ordered_index, include)
+
+	var texture_array := Texture2DArray.new()
+	if textures.is_empty():
+		return image
+	texture_array.create_from_images(textures)
+	var params := {
+		"layers": texture_array,
+		"metadata": ImageTexture.create_from_image(metadata_image),
+	}
+	var blended := Image.create_empty(project.size.x, project.size.y, false, image.get_format())
+	var gen := ShaderImageEffect.new()
+	gen.generate_image(blended, DrawingAlgos.blend_layers_shader, params, project.size)
+	image.blend_rect(blended, Rect2i(Vector2.ZERO, project.size), Vector2.ZERO)
+	# Re-order the layers again to ensure correct canvas drawing
+	project.ordered_layers = previous_ordered_layers
+	return image
+
+
+## modified version of `set_layer_metadata_image` in DrawingAlgos
+func _set_layer_metadata_image(
+	layer, cel, image, index, include := true
+) -> void:
+	# Store the blend mode
+	image.set_pixel(index, 0, Color(layer.blend_mode / 255.0, 0.0, 0.0, 0.0))
+	# Store the opacity
+	if layer.is_visible_in_hierarchy():
+		var opacity = cel.get_final_opacity(layer)
+		image.set_pixel(index, 1, Color(opacity, 0.0, 0.0, 0.0))
 	else:
-		textures.append(blended_children)
-		_cache_texture_data.append(blended_children.get_data())
-		DrawingAlgos.set_layer_metadata_image(layer, cel, metadata_image, i)
-	new_i += 1
-	return new_i
+		image.set_pixel(index, 1, Color())
+	# Store the clipping mask boolean
+	if layer.clipping_mask:
+		image.set_pixel(index, 3, Color.RED)
+	else:
+		image.set_pixel(index, 3, Color.BLACK)
+	if not include:
+		# Store a small red value as a way to indicate that this layer should be skipped
+		# Used for layers such as child layers of a group, so that the group layer itself can
+		# successfully be used as a clipping mask with the layer below it.
+		image.set_pixel(index, 3, Color(0.2, 0.0, 0.0, 0.0))
+
+
+func apply_bone(cel_image: ImageExtended, at_frame: Frame) -> Image:
+	if name == "Group 2":
+		cel_image.save_png("out/a")
+	if not enabled:
+		return cel_image
+	var bone_cel: BoneCel = at_frame.cels[index]
+	var used_region := cel_image.get_used_rect()
+	var start_point: Vector2i = bone_cel.start_point
+	var gizmo_origin: Vector2i = bone_cel.gizmo_origin.floor()
+	var angle: float = bone_cel.bone_rotation
+	if angle == 0 and start_point == Vector2i.ZERO:
+		return cel_image
+	if used_region.size == Vector2i.ZERO:
+		return cel_image
+	# Imprint on a square for rotation
+	# (We are doing this so that the image doesn't get clipped as a result of rotation.)
+	var diagonal_length := floori(used_region.size.length())
+	if diagonal_length % 2 == 0:
+		diagonal_length += 1
+	var s_offset: Vector2i = (
+		0.5 * (Vector2i(diagonal_length, diagonal_length)
+		- used_region.size)
+	).floor()
+	var square_image = cel_image.get_region(
+		Rect2i(used_region.position - s_offset, Vector2i(diagonal_length, diagonal_length))
+	)
+	# Apply Rotation To this Image
+	if angle != 0:
+		var transformation_matrix := Transform2D(angle, Vector2.ZERO)
+		var rotate_params := {
+			"transformation_matrix": transformation_matrix.affine_inverse(),
+			"pivot": Vector2(0.5, 0.5),
+			"ending_angle": angle,
+			"tolerance": 0,
+			"preview": false
+		}
+		# Detects if the rotation is changed for this generation or not
+		# (useful if bone is moved arround while having some rotation)
+		# NOTE: I tried cacheing entire poses (that remain same) as well. It was faster than this
+		# approach but only by a few milliseconds. I don't think straining the memory for only
+		# a boost of a few millisec was worth it so i declare this the most optimal approach.
+		var cache_key := {"angle": angle, "cel_content": cel_image.get_data()}
+		var bone_cache: Dictionary = generation_cache.get_or_add(bone_cel, {})
+		if cache_key in bone_cache.keys():
+			square_image = bone_cache[cache_key]
+		else:
+			var gen = ShaderImageEffect.new()
+			gen.generate_image(
+				square_image,
+				DrawingAlgos.nn_shader,
+				rotate_params, square_image.get_size()
+			)
+			bone_cache.clear()
+			bone_cache[cache_key] = square_image
+	if name == "Group 2":
+		square_image.save_png("out/test")
+		cel_image.save_png("out/cel %s" % name)
+	var pivot: Vector2i = gizmo_origin
+	var bone_start_global: Vector2i = gizmo_origin + start_point
+	var square_image_start: Vector2i = used_region.position - s_offset
+	var global_square_centre: Vector2 = square_image_start + (square_image.get_size() / 2)
+	var global_rotated_new_centre = (
+		(global_square_centre - Vector2(pivot)).rotated(angle)
+		+ Vector2(bone_start_global)
+	)
+	var new_start: Vector2i = (
+		square_image_start
+		+ Vector2i((global_rotated_new_centre - global_square_centre).floor())
+	)
+	cel_image.fill(Color(0, 0, 0, 0))
+	cel_image.blit_rect(
+		square_image,
+		Rect2i(Vector2.ZERO, square_image.get_size()),
+		Vector2i(new_start)
+	)
+	return cel_image
 
 
 func get_layer_type() -> int:
