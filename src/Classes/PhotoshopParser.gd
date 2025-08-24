10df9c5e319b373c912c02cea4b2a5bacb7aec63
Emmanouil Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2025-08-24 14:40:03 +0300

    Refactor PhotoshopParser.gd and start working on animation frame support
    Animation data is being read but is not yet taken into account when creating the Pixelorama project as of this commit.

--- src/Classes/PhotoshopParser.gd
+++ src/Classes/PhotoshopParser.gd
@@ -18,7 +18,16 @@ const PSB_EIGHT_BYTE_ADDITIONAL_LAYER_KEYS: PackedStringArray = [
 ]
 
 
+class PhotoshopProject:
+	var layers: Array[Dictionary] = []
+	var guides: Array[Dictionary] = []
+	var frames := {}
+	var size := Vector2i()
+	var path: String
+
+
 # https://www.adobe.com/devnet-apps/photoshop/fileformatashtml/
+# https://github.com/layervault/psd.rb/wiki/Anatomy-of-a-PSD-File
 # https://github.com/gaoyan2659365465/Godot4Library/blob/main/addons/psd/psd.gd
 # gdlint: disable=function-variable-name
 static func open_photoshop_file(path: String) -> void:
@@ -48,6 +57,7 @@ static func open_photoshop_file(path: String) -> void:
 	var guides: Array[Dictionary] = []
 	# Image Resources
 	var image_resources_length := psd_file.get_32()
+	var frames := {}
 	if image_resources_length > 0:
 		var data_start := psd_file.get_position()
 		var data_end := data_start + image_resources_length
@@ -63,24 +73,48 @@ static func open_photoshop_file(path: String) -> void:
 				psd_file.get_8()  # Padding byte
 
 			var size := psd_file.get_32()
-			var data := psd_file.get_buffer(size)
-			if size % 2 != 0:
-				psd_file.get_8()  # Padding byte
-			if resource_id == 1032:  # Grid and guides
-				var gg_version_buffer := data.slice(0, 4)
-				gg_version_buffer.reverse()
-				var _gg_version := gg_version_buffer.decode_s32(0)
-				var guide_count_buffer := data.slice(12, 16)
-				guide_count_buffer.reverse()
-				var guide_count := guide_count_buffer.decode_s32(0)
-				var byte_index := 16
-				for i in guide_count:
-					var guide_location_buffer := data.slice(byte_index, byte_index + 4)
-					guide_location_buffer.reverse()
-					var guide_location := guide_location_buffer.decode_s32(0)
-					var guide_direction := data[byte_index + 4]
-					byte_index += 5
-					guides.append({"position": guide_location / 32.0, "direction": guide_direction})
+			if resource_id == 4000:  # Plug-in resource
+				var plugin_data_start := psd_file.get_position()
+				psd_file.get_buffer(12)  # Not sure what these are for
+				var plugin_signature := psd_file.get_buffer(4).get_string_from_ascii()
+				if plugin_signature == "8BIM":
+					var plugin_key := psd_file.get_buffer(4).get_string_from_ascii()
+					if plugin_key == "AnDs":  # Read frame data
+						# Not sure if these are indeed a size and a version
+						# because this part is not documented, but it would make the most sense.
+						var _ands_size := psd_file.get_32()
+						var _ands_version := psd_file.get_32()  # Seems to be 16.
+						var descriptor := parse_descriptor(psd_file)
+						if descriptor.has("FrIn"):
+							var frin: Array = descriptor["FrIn"]
+							for frame in frin:
+								if not frame.has("FrID") or not frame.has("FrDl"):
+									continue
+								var frame_id = frame["FrID"]
+								var frame_delay = frame["FrDl"]
+								frames[frame_id] = {"delay": frame_delay, "layer_data": {}}
+				psd_file.seek(plugin_data_start + ((size + 1) & ~1))
+			else:
+				var data := psd_file.get_buffer(size)
+				if resource_id == 1032:  # Grid and guides
+					var gg_version_buffer := data.slice(0, 4)
+					gg_version_buffer.reverse()
+					var _gg_version := gg_version_buffer.decode_s32(0)
+					var guide_count_buffer := data.slice(12, 16)
+					guide_count_buffer.reverse()
+					var guide_count := guide_count_buffer.decode_s32(0)
+					var byte_index := 16
+					for i in guide_count:
+						var guide_location_buffer := data.slice(byte_index, byte_index + 4)
+						guide_location_buffer.reverse()
+						var guide_location := guide_location_buffer.decode_s32(0)
+						var guide_direction := data[byte_index + 4]
+						byte_index += 5
+						guides.append(
+							{"position": guide_location / 32.0, "direction": guide_direction}
+						)
+				if size % 2 != 0:
+					psd_file.get_8()  # Padding byte
 		psd_file.seek(data_end)
 	# Layer and Mask Information Section
 	var _layer_and_mask_info_section_length: int
@@ -102,6 +136,7 @@ static func open_photoshop_file(path: String) -> void:
 	# Layer records
 	for i in layer_count:
 		var layer := {}
+		layer.index = i
 		layer.top = get_signed_32(psd_file)
 		layer.left = get_signed_32(psd_file)
 		layer.bottom = get_signed_32(psd_file)
@@ -184,12 +219,32 @@ static func open_photoshop_file(path: String) -> void:
 						# 0 = normal, 1 = scene group, affects the animation timeline.
 						var _sub_type := psd_file.get_32()
 			elif key == "luni":
-				# Unicode layer name (UTF-16 string length, then UTF-16 content)
-				name_length = psd_file.get_32()
-				var _name_utf16 := psd_file.get_buffer(name_length * 2)
-				#layer.name = name_utf16.get_string_from_utf16()
+				var unicode_layer_name := parse_unicode_string(psd_file)
+				layer.name = unicode_layer_name
 			elif key == "lclr":
 				layer.color = parse_lclr_block(psd_file.get_buffer(8))
+			elif key == "shmd":
+				var metadata_items := psd_file.get_32()
+				for metadata_item in metadata_items:
+					var _metadata_signature := psd_file.get_buffer(4).get_string_from_utf8()
+					var metadata_key := psd_file.get_buffer(4).get_string_from_utf8()
+					var _metadata_copy_on_sheet_duplication := psd_file.get_8()
+					var _metadata_padding := psd_file.get_buffer(3)
+					var metadata_length := psd_file.get_32()
+					var metadata_start := psd_file.get_position()
+					if metadata_key == "mlst":
+						var _mlst_version := psd_file.get_32()  # Should be equal to 16.
+						var descriptor := parse_descriptor(psd_file)
+						if descriptor.has("LaSt"):
+							var layer_state: Array = descriptor["LaSt"]
+							for layer_state_frame in layer_state:
+								if layer_state_frame.has("FrLs"):
+									var frame_id = layer_state_frame["FrLs"][0]
+									if not frames.has(frame_id):
+										frames[frame_id] = {"layer_data": {}}
+									var layer_enabled = layer_state_frame.get("enab", layer.visible)
+									frames[frame_id]["layer_data"][layer.index] = layer_enabled
+					psd_file.seek(metadata_start + ((metadata_length + 1) & ~1))
 
 			# Move to next block (align length to even)
 			psd_file.seek(data_start + ((length + 1) & ~1))
@@ -203,11 +258,30 @@ static func open_photoshop_file(path: String) -> void:
 			channel.data_offset = psd_file.get_position()
 			psd_file.seek(psd_file.get_position() + channel.length)
 
-	var project_size := Vector2i(width, height)
-	var new_project := Project.new([], path.get_file().get_basename(), project_size)
+	# Decode images in the PSD's layers.
+	# This is the most important part of the whole process.
+	for layer in psd_layers:
+		if layer.group_type == "layer":
+			var image := decode_psd_layer(psd_file, layer, is_psb)
+			if is_instance_valid(image) and not image.is_empty():
+				layer.image = image
+
+	psd_file.close()
+	var psd_project := PhotoshopProject.new()
+	psd_project.size = Vector2i(width, height)
+	psd_project.layers = psd_layers
+	psd_project.frames = frames
+	psd_project.guides = guides
+	psd_project.path = path
+	psd_to_pxo_project(psd_project)
+
+
+static func psd_to_pxo_project(psd_project: PhotoshopProject) -> void:
+	var project_size := psd_project.size
+	var new_project := Project.new([], psd_project.path.get_file().get_basename(), project_size)
 	var frame := Frame.new()
 	var layer_index := 0
-	for psd_layer in psd_layers:
+	for psd_layer in psd_project.layers:
 		if psd_layer.group_type == "end":
 			continue
 		if psd_layer.group_type.begins_with("start"):
@@ -235,9 +309,9 @@ static func open_photoshop_file(path: String) -> void:
 			layer.set_meta(&"layer_child_level", psd_layer.layer_child_level)
 			new_project.layers.append(layer)
 			layer_index += 1
-			var image := decode_psd_layer(psd_file, psd_layer, is_psb)
+			var image: Image = psd_layer.get("image", null)
 			if is_instance_valid(image) and not image.is_empty():
-				image.crop(width, height)
+				image.crop(project_size.x, project_size.y)
 				var img_copy := Image.new()
 				img_copy.copy_from(image)
 				image.fill(Color(0, 0, 0, 0))
@@ -249,7 +323,6 @@ static func open_photoshop_file(path: String) -> void:
 				var cel := layer.new_empty_cel()
 				frame.cels.append(cel)
 
-	psd_file.close()
 	if new_project.layers.size() == 0:
 		var layer := PixelLayer.new(new_project)
 		layer.index = 0
@@ -259,7 +332,7 @@ static func open_photoshop_file(path: String) -> void:
 	organize_layer_child_levels(new_project)
 	new_project.frames.append(frame)
 	new_project.order_layers()
-	for psd_guide in guides:
+	for psd_guide in psd_project.guides:
 		var guide := Guide.new()
 		if psd_guide.direction == 0:
 			guide.type = Guide.Types.VERTICAL
@@ -439,6 +512,84 @@ static func parse_lclr_block(buffer: PackedByteArray) -> Color:
 			return Color.GRAY
 		_:
 			return Color(0, 0, 0, 0)
+
+
+static func parse_unicode_string(f: FileAccess) -> String:
+	var length := f.get_32()
+	if length == 0:
+		return ""
+	var bytes := f.get_buffer(length * 2)
+	var s := ""
+	for i in range(length):
+		var hi := int(bytes[i * 2])
+		var lo := int(bytes[i * 2 + 1])
+		s += char((hi << 8) | lo)
+	return s
+
+
+static func parse_class_id(f: FileAccess) -> String:
+	var length := f.get_32()
+	if length == 0:
+		return f.get_buffer(4).get_string_from_utf8()
+	return f.get_buffer(length).get_string_from_utf8()
+
+
+static func parse_descriptor(f: FileAccess) -> Dictionary:
+	var desc := {}
+	var _name := parse_unicode_string(f)
+	var _class_id := parse_class_id(f)
+	var item_count := f.get_32()
+	for i in range(item_count):
+		var key := parse_class_id(f)
+		var type_key := f.get_buffer(4).get_string_from_utf8()
+		var value = parse_descriptor_value(f, type_key)
+		desc[key] = value
+	return desc
+
+
+static func parse_descriptor_value(f: FileAccess, type_key: String) -> Variant:
+	match type_key:
+		"long":
+			return f.get_32()
+		"comp":
+			return f.get_64()
+		"doub":
+			return f.get_double()
+		"bool":
+			return f.get_8() != 0
+		"TEXT":
+			return parse_unicode_string(f)
+		"enum":
+			var type := parse_class_id(f)  # enum type
+			var value := parse_class_id(f)  # enum value
+			return {"enum_type": type, "enum_value": value}
+		"Objc", "GlbO":
+			return parse_descriptor(f)
+		"VlLs":
+			var count := f.get_32()
+			var arr := []
+			for i in range(count):
+				var key := f.get_buffer(4).get_string_from_utf8()
+				arr.append(parse_descriptor_value(f, key))
+			return arr
+		"UntF":
+			return parse_unit(f, true)
+		"UnFl":
+			return parse_unit(f, false)
+		_:
+			# Skip unknown types
+			print("Unknown descriptor type:", type_key)
+			return null
+
+
+static func parse_unit(f: FileAccess, double := true) -> Dictionary:
+	var unit_id := f.get_buffer(4).get_string_from_utf8()
+	var value: float
+	if double:
+		value = f.get_double()
+	else:
+		value = f.get_float()
+	return {"id": unit_id, "value": value}
 
 
 static func organize_layer_child_levels(project: Project) -> void:
