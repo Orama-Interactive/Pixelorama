aae5f56e1f5b44530ce0c5c6e7a144ab31df62fd
Variable-ind <77773850+Variable-ind@users.noreply.github.com>
2025-09-04 19:11:45 +0500

    Allow creating resource projects through Api

--- src/Classes/ResourceProject.gd
+++ src/Classes/ResourceProject.gd
@@ -1,8 +1,15 @@
 class_name ResourceProject
 extends Project
+## A class for easily editing individual project sub-resources like tiles, index maps, etc.
+##
+## The [ResourceProject] is basically a [Project], except that it doesn't get saved physically
+## (as a .pxo file), instead, a [signal resource_updated] signal is emitted which can
+## be used to update the resource in the [Project].[br]
 
+
+## Emitted when the [ResourceProject] is saved.
 @warning_ignore("unused_signal")
-signal resource_updated
+signal resource_updated(project: Project)
 
 
 func _init(_frames: Array[Frame] = [], _name := tr("untitled"), _size := Vector2i(64, 64)) -> void:
