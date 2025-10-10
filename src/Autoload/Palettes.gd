fe6f61db66a68e32ad5c6a7874474a9a80ba65b9
Emmanouil Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2025-10-10 18:10:22 +0300

    Handle lospec palette loading the same way we handle palette loading from files

--- src/Autoload/Palettes.gd
+++ src/Autoload/Palettes.gd
@@ -728,18 +728,13 @@ func _on_lospec_palette_downloaded(
 	var data = JSON.parse_string(body.get_string_from_utf8())
 	if not data or not typeof(data) == TYPE_DICTIONARY:
 		push_error("Palette download failed")
-	var colors = data.colors
-	# Convert hex colors to Color() objects and add to Pixelorama's palette
-	var pixelorama_json_colors := []
-	var palette := []
-	for i in colors.size():
-		var color_hex = colors[i]
+	var colors_hex = data.colors
+	var colors: PackedColorArray = []
+	for color_hex in colors_hex:
 		var color := Color(color_hex)
-		pixelorama_json_colors.append({"color": color, "index": i})
-	var pixelorama_json := {
-		"comment": data["author"],
-		"width": colors.size(),
-		"height": 1,
-		"colors": pixelorama_json_colors
-	}
-	ExtensionsApi.palette.create_palette_from_data(data["name"], pixelorama_json)
+		colors.append(color)
+	var palette := fill_imported_palette_with_colors(data["name"], colors, data["author"])
+	save_palette(palette)
+	palettes[palette.name] = palette
+	select_palette(palette.name)
+	new_palette_created.emit()
