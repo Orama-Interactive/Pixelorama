5c80da006e84f1480f458a14ae44267f28c5bec9
Emmanouil Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2025-10-15 19:46:40 +0300

    Minor code cleaning in FrameScrollContainer.gd

--- src/UI/Timeline/FrameScrollContainer.gd
+++ src/UI/Timeline/FrameScrollContainer.gd
@@ -13,45 +13,44 @@ func _ready() -> void:
 
 
 func _gui_input(event: InputEvent) -> void:
-	if get_child_count():
-		var vertical_scroll: bool = get_child(0).size.y >= size.y
-		var should_h_scroll := not vertical_scroll
-		if event is InputEventWithModifiers and not should_h_scroll:
-			should_h_scroll = event.shift_pressed
-		if event is InputEventMouseButton:
-			if event.button_index in [MOUSE_BUTTON_WHEEL_RIGHT, MOUSE_BUTTON_WHEEL_LEFT]:
-				# This helps/allows two finger scrolling (on Laptops)
-				should_h_scroll = true
-			if should_h_scroll:
-				if is_instance_valid(h_scroll_bar):
-					if (
-						event.button_index == MOUSE_BUTTON_WHEEL_UP
-						or event.button_index == MOUSE_BUTTON_WHEEL_LEFT
-					):
-						h_scroll_bar.value -= Global.animation_timeline.cel_size / 2 + 2
-						accept_event()
-					elif (
-						event.button_index == MOUSE_BUTTON_WHEEL_DOWN
-						or event.button_index == MOUSE_BUTTON_WHEEL_RIGHT
-					):
-						h_scroll_bar.value += Global.animation_timeline.cel_size / 2 + 2
-						accept_event()
-		# Pan scrolling, used by some MacOS devices
-		# (see: https://github.com/Orama-Interactive/Pixelorama/discussions/1218)
-		elif event is InputEventPanGesture:
-			if event.delta.x != 0:
-				h_scroll_bar.value += (
-					sign(event.delta.x) * (Global.animation_timeline.cel_size / 2 + 2)
-				)
+	if get_child_count() == 0:
+		return
+	var vertical_scroll: bool = get_child(0).size.y >= size.y
+	var should_h_scroll := not vertical_scroll
+	var cel_size: int = Global.animation_timeline.cel_size / 2 + 2
+	if event is InputEventWithModifiers and not should_h_scroll:
+		should_h_scroll = event.shift_pressed
+	if event is InputEventMouseButton:
+		if event.button_index in [MOUSE_BUTTON_WHEEL_RIGHT, MOUSE_BUTTON_WHEEL_LEFT]:
+			# This helps/allows two finger scrolling (on Laptops)
+			should_h_scroll = true
+		if should_h_scroll:
+			if is_instance_valid(h_scroll_bar):
+				if (
+					event.button_index == MOUSE_BUTTON_WHEEL_UP
+					or event.button_index == MOUSE_BUTTON_WHEEL_LEFT
+				):
+					h_scroll_bar.value -= cel_size
+					accept_event()
+				elif (
+					event.button_index == MOUSE_BUTTON_WHEEL_DOWN
+					or event.button_index == MOUSE_BUTTON_WHEEL_RIGHT
+				):
+					h_scroll_bar.value += cel_size
+					accept_event()
+	# Pan scrolling, used by some MacOS devices
+	# (see: https://github.com/Orama-Interactive/Pixelorama/discussions/1218)
+	elif event is InputEventPanGesture:
+		if event.delta.x != 0:
+			h_scroll_bar.value += signf(event.delta.x) * cel_size
 
 
 func _update_scroll() -> void:
-	if get_child_count():
-		if is_instance_valid(h_scroll_bar):
-			h_scroll_bar.max_value = get_child(0).size.x
-			h_scroll_bar.page = size.x
-			h_scroll_bar.visible = h_scroll_bar.page < h_scroll_bar.max_value
-			get_child(0).position.x = -h_scroll_bar.value + PADDING
+	if get_child_count() > 0 and is_instance_valid(h_scroll_bar):
+		h_scroll_bar.max_value = get_child(0).size.x
+		h_scroll_bar.page = size.x
+		h_scroll_bar.visible = h_scroll_bar.page < h_scroll_bar.max_value
+		get_child(0).position.x = -h_scroll_bar.value + PADDING
 
 
 func ensure_control_visible(control: Control) -> void:
