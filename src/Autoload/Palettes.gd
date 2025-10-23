Orama-Interactive/Pixelorama

commit f6abe2f1080730ba9de8da58d85a247d137b542c

In BaseDraw, move undo_redo.create_action to commit_undo
The reason it got placed to prepare_undo was to fix a bug where the user could undo in the middle of drawing, in #603. However, almost one year later, I introduced another fix for this issue which affected all tools, not just the design ones, in af2b1feb1f63144ebce00520ea2f8ee832dc49bd. Therefore the fix in #603 is no longer needed. I did some tests and nothing seems broken, but we should keep it in mind just in case.

src/Tools/BaseDraw.gd
@@ -248,13 +248,11 @@ func update_line_polylines(start: Vector2i, end: Vector2i) -> void:
 	_line_polylines = _create_polylines(indicator)
 
 
-func prepare_undo(action: String) -> void:
-	var project := Global.current_project
+func prepare_undo() -> void:
 	_undo_data = _get_undo_data()
-	project.undo_redo.create_action(action)
 
 
-func commit_undo() -> void:
+func commit_undo(action := "Draw") -> void:
 	var project := Global.current_project
 	Global.canvas.update_selected_cels_textures(project)
 	var tile_editing_mode := TileSetPanel.tile_editing_mode
@@ -269,6 +267,7 @@ func commit_undo() -> void:
 		frame = project.current_frame
 		layer = project.current_layer
 
+	project.undo_redo.create_action(action)
 	project.deserialize_cel_undo_data(redo_data, _undo_data)
 	project.undo_redo.add_do_method(Global.undo_or_redo.bind(false, frame, layer))
 	project.undo_redo.add_undo_method(Global.undo_or_redo.bind(true, frame, layer))

src/Tools/BaseShapeDrawer.gd
@@ -180,7 +180,7 @@ func draw_preview() -> void:
 func _draw_shape(origin: Vector2i, dest: Vector2i) -> void:
 	var rect := _get_result_rect(origin, dest)
 	var points := _get_points(rect.size)
-	prepare_undo("Draw Shape")
+	prepare_undo()
 	var images := _get_selected_draw_images()
 	var thickness_vector := rect.position - Vector2i((Vector2(0.5, 0.5) * (_thickness - 1)).ceil())
 	for point in points:
@@ -195,7 +195,7 @@ func _draw_shape(origin: Vector2i, dest: Vector2i) -> void:
 				for image in images:
 					_drawer.set_pixel(image, draw_pos, tool_slot.color)
 
-	commit_undo()
+	commit_undo("Draw Shape")
 
 
 ## Given an origin point and destination point, returns a rect representing

src/Tools/DesignTools/CurveTool.gd
@@ -238,7 +238,7 @@ func draw_preview() -> void:
 func _draw_shape() -> void:
 	bezier_option_button.disabled = false
 	var points := _bezier()
-	prepare_undo("Draw Shape")
+	prepare_undo()
 	var images := _get_selected_draw_images()
 	for point in points:
 		# Reset drawer every time because pixel perfect sometimes breaks the tool
@@ -255,7 +255,7 @@ func _draw_shape() -> void:
 				if Geometry2D.is_point_in_polygon(v, points):
 					_draw_pixel(v, images)
 	_clear()
-	commit_undo()
+	commit_undo("Draw Shape")
 
 
 func _draw_pixel(point: Vector2i, images: Array[ImageExtended]) -> void:

src/Tools/DesignTools/Eraser.gd
@@ -44,7 +44,7 @@ func draw_start(pos: Vector2i) -> void:
 		return
 	_picking_color = false
 	Global.canvas.selection.transform_content_confirm()
-	prepare_undo("Draw")
+	prepare_undo()
 	update_mask(_strength == 1)
 	_changed = false
 	_drawer.color_op.changed = false

src/Tools/DesignTools/IsometricBoxTool.gd
@@ -207,7 +207,7 @@ func draw_preview() -> void:
 
 	var box_points = _control_pts.duplicate()
 	box_points.push_front(_origin)
-	var canvas = Global.canvas.previews
+	var canvas := Global.canvas.previews
 
 	canvas.draw_set_transform(Vector2(0.5, 0.5))
 	for i: int in box_points.size():
@@ -219,11 +219,13 @@ func draw_preview() -> void:
 		canvas.draw_line(Vector2.UP * 0.5, Vector2.DOWN * 0.5, Color.WHITE)
 		canvas.draw_line(Vector2.RIGHT * 0.5, Vector2.LEFT * 0.5, Color.WHITE)
 	if box_points.size() in [2, 4]:
-		var current_pixel = Global.canvas.current_pixel.floor()
+		var current_pixel := Global.canvas.current_pixel.floor()
 		current_pixel = box_constraint(_last_pixel, current_pixel, _current_state)
-		var length = int(current_pixel.distance_to(box_points[-1]))
-		var prefix = "Corner" if box_points.size() == 2 else "Height"
-		var str_val = str(prefix, ": ", length + 1 if box_points.size() == 2 else length, " ", "px")
+		var length := int(current_pixel.distance_to(box_points[-1]))
+		var prefix := "Corner" if box_points.size() == 2 else "Height"
+		var str_val := str(
+			prefix, ": ", length + 1 if box_points.size() == 2 else length, " ", "px"
+		)
 		# We are using the measurementsnode for measurement based previews.
 		Global.canvas.measurements.draw.connect(
 			_preview_updater.bind(current_pixel, box_points[-1], str_val)
@@ -264,7 +266,7 @@ func _preview_updater(point_a: Vector2, point_b: Vector2, str_value: String) -> 
 
 func _draw_shape() -> void:
 	_drawing = false
-	prepare_undo("Draw Shape")
+	prepare_undo()
 	var images := _get_selected_draw_images()
 	if _fill_inside and !Tools.is_placing_tiles():
 		# converting control points to local basis vectors
@@ -351,7 +353,7 @@ func _draw_shape() -> void:
 				# Draw each point offsetted based on the shape's thickness
 				_draw_pixel(point, images)
 	_clear()
-	commit_undo()
+	commit_undo("Draw Shape")
 
 
 func _draw_pixel(point: Vector2i, images: Array[ImageExtended]) -> void:

src/Tools/DesignTools/LineTool.gd
@@ -168,7 +168,7 @@ func draw_preview() -> void:
 
 func _draw_shape() -> void:
 	var points := bresenham_line_thickness(_start, _dest, _thickness)
-	prepare_undo("Draw Shape")
+	prepare_undo()
 	var images := _get_selected_draw_images()
 	for point in points:
 		# Reset drawer every time because pixel perfect sometimes breaks the tool
@@ -181,7 +181,7 @@ func _draw_shape() -> void:
 				for image in images:
 					_drawer.set_pixel(image, point, tool_slot.color)
 
-	commit_undo()
+	commit_undo("Draw Shape")
 
 
 func _line_angle_constraint(start: Vector2, end: Vector2) -> Dictionary:

src/Tools/DesignTools/Pencil.gd
@@ -103,7 +103,7 @@ func draw_start(pos: Vector2i) -> void:
 	_picking_color = false
 
 	Global.canvas.selection.transform_content_confirm()
-	prepare_undo("Draw")
+	prepare_undo()
 	var can_skip_mask := true
 	if tool_slot.color.a < 1 and !_overwrite:
 		can_skip_mask = false

src/Tools/DesignTools/Shading.gd
@@ -247,7 +247,7 @@ func draw_start(pos: Vector2i) -> void:
 	_changed = false
 	_drawer.color_op.changed = false
 
-	prepare_undo("Draw")
+	prepare_undo()
 	_drawer.reset()
 
 	_draw_line = Input.is_action_pressed("draw_create_line")

commit 1df8982cb7406b4bd54faa0ee9b4ea31a05dd286

Fix global palette not turning into project palette after drawing
If the color existed on the palette, or auto add colors was enabled. The bug was introduced in f6abe2f1080730ba9de8da58d85a247d137b542c

src/Autoload/Palettes.gd
@@ -128,7 +128,7 @@ func add_palette_as_project_palette(new_palette: Palette) -> void:
 
 
 func undo_redo_add_palette(new_palette: Palette):
-	var undo_redo = Global.current_project.undo_redo
+	var undo_redo := Global.current_project.undo_redo
 	undo_redo.add_do_method(add_palette_as_project_palette.bind(new_palette))
 	undo_redo.add_undo_method(palette_delete_and_reselect.bind(true, new_palette))
 	undo_redo.add_undo_method(select_palette.bind(current_palette.name))

src/Tools/BaseDraw.gd
@@ -258,7 +258,6 @@ func commit_undo(action := "Draw") -> void:
 	var tile_editing_mode := TileSetPanel.tile_editing_mode
 	if TileSetPanel.placing_tiles:
 		tile_editing_mode = TileSetPanel.TileEditingMode.STACK
-	manage_undo_redo_palettes()
 	project.update_tilemaps(_undo_data, tile_editing_mode)
 	var redo_data := _get_undo_data()
 	var frame := -1
@@ -268,6 +267,7 @@ func commit_undo(action := "Draw") -> void:
 		layer = project.current_layer
 
 	project.undo_redo.create_action(action)
+	manage_undo_redo_palettes()
 	project.deserialize_cel_undo_data(redo_data, _undo_data)
 	project.undo_redo.add_do_method(Global.undo_or_redo.bind(false, frame, layer))
 	project.undo_redo.add_undo_method(Global.undo_or_redo.bind(true, frame, layer))
@@ -277,7 +277,7 @@ func commit_undo(action := "Draw") -> void:
 
 
 ## Manages conversion of global palettes into local if a drawable tool is used
-func manage_undo_redo_palettes():
+func manage_undo_redo_palettes() -> void:
 	if _is_eraser:
 		return
 	var palette_in_focus := Palettes.current_palette