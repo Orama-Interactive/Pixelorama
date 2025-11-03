Orama-Interactive/Pixelorama

commit 5e32237a9f603e0fe62dd364c6c13592104a4269

Only set cursor shape once in DockableContainer's split handles
This also fixes an issue in touch screens where touching on a split handle was not being emulated as a mouse button press

addons/dockable_container/split_handle.gd
@@ -20,6 +20,13 @@ var _mouse_hovering := false
 var _dragging := false
 
 
+func _ready() -> void:
+	await get_tree().process_frame
+	if not is_instance_valid(layout_split):
+		return
+	mouse_default_cursor_shape = SPLIT_MOUSE_CURSOR_SHAPE[layout_split.direction]
+
+
 func _draw() -> void:
 	var theme_class := SPLIT_THEME_CLASS[layout_split.direction]
 	var icon := get_theme_icon("grabber", theme_class)
@@ -50,12 +57,10 @@ func _gui_input(event: InputEvent) -> void:
 func _notification(what: int) -> void:
 	if what == NOTIFICATION_MOUSE_ENTER:
 		_mouse_hovering = true
-		set_split_cursor(true)
 		if bool(get_theme_constant("autohide", SPLIT_THEME_CLASS[layout_split.direction])):
 			queue_redraw()
 	elif what == NOTIFICATION_MOUSE_EXIT:
 		_mouse_hovering = false
-		set_split_cursor(false)
 		if bool(get_theme_constant("autohide", SPLIT_THEME_CLASS[layout_split.direction])):
 			queue_redraw()
 	elif what == NOTIFICATION_FOCUS_EXIT:
@@ -76,13 +81,6 @@ func get_layout_minimum_size() -> Vector2:
 			maxf(first_minimum_size.x, second_minimum_size.x),
 			first_minimum_size.y + separation + second_minimum_size.y
 		)
-
-
-func set_split_cursor(value: bool) -> void:
-	if value:
-		mouse_default_cursor_shape = SPLIT_MOUSE_CURSOR_SHAPE[layout_split.direction]
-	else:
-		mouse_default_cursor_shape = CURSOR_ARROW
 
 
 func get_split_rects(rect: Rect2) -> Dictionary:

src/Main.tscn
@@ -26,6 +26,7 @@ anchor_right = 1.0
 anchor_bottom = 1.0
 grow_horizontal = 2
 grow_vertical = 2
+mouse_filter = 1
 theme = ExtResource("1")
 script = ExtResource("2")
 

src/UI/UI.tscn
@@ -220,6 +220,7 @@ anchor_right = 1.0
 anchor_bottom = 1.0
 size_flags_horizontal = 3
 size_flags_vertical = 3
+mouse_filter = 1
 script = ExtResource("37")
 
 [node name="DockableContainer" type="Container" parent="."]
@@ -304,7 +305,8 @@ render_target_update_mode = 4
 
 [node name="TransparentChecker" parent="DockableContainer/Main Canvas/ViewportandVerticalRuler/SubViewportContainer/SubViewport" instance=ExtResource("5")]
 material = SubResource("2")
-anchors_preset = 0
+layout_direction = 2
+mouse_filter = 2
 
 [node name="Canvas" parent="DockableContainer/Main Canvas/ViewportandVerticalRuler/SubViewportContainer/SubViewport" instance=ExtResource("19")]
 
@@ -340,7 +342,8 @@ render_target_update_mode = 0
 
 [node name="TransparentChecker" parent="DockableContainer/Second Canvas/SubViewport" instance=ExtResource("5")]
 material = SubResource("3")
-anchors_preset = 0
+layout_direction = 2
+mouse_filter = 2
 
 [node name="CanvasPreview" parent="DockableContainer/Second Canvas/SubViewport" groups=["CanvasPreviews"] instance=ExtResource("2")]
 

commit 5f7779d9b358f29244d0dbf20255e975b9e42676

Fix dockable container split handle having the wrong cursor shape
Regression from 5e32237a9f603e0fe62dd364c6c13592104a4269

addons/dockable_container/split_handle.gd
@@ -11,20 +11,17 @@ const SPLIT_MOUSE_CURSOR_SHAPE: Array[Control.CursorShape] = [
 	Control.CURSOR_VSPLIT,  # SPLIT_MOUSE_CURSOR_SHAPE[DockableLayoutSplit.Direction.VERTICAL]
 ]
 
-var layout_split: DockableLayoutSplit
+var layout_split: DockableLayoutSplit:
+	set(value):
+		layout_split = value
+		if is_instance_valid(layout_split):
+			mouse_default_cursor_shape = SPLIT_MOUSE_CURSOR_SHAPE[layout_split.direction]
 var first_minimum_size: Vector2
 var second_minimum_size: Vector2
 
 var _parent_rect: Rect2
 var _mouse_hovering := false
 var _dragging := false
-
-
-func _ready() -> void:
-	await get_tree().process_frame
-	if not is_instance_valid(layout_split):
-		return
-	mouse_default_cursor_shape = SPLIT_MOUSE_CURSOR_SHAPE[layout_split.direction]
 
 
 func _draw() -> void: