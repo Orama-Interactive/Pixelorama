f6d51d08badca6d1c26a85db61714e7c1940b3ff
Emmanouil Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2025-12-11 12:03:59 +0200

    Improve cursor moving logic

--- src/UI/Timeline/KeyframeTimeline/keyframe_timeline_cursor.gd
+++ src/UI/Timeline/KeyframeTimeline/keyframe_timeline_cursor.gd
@@ -10,32 +10,34 @@ var is_dragged := false
 
 
 func _ready() -> void:
-	gui_input.connect(_on_gui_input)
+	if is_instance_valid(container):
+		container.get_child(0).gui_input.connect(_on_frames_container_gui_input)
 
 
-func _on_gui_input(event: InputEvent) -> void:
+func _on_frames_container_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
 			is_dragged = true
+			_update_position(event.global_position)
 		else:
 			is_dragged = false
 	if event is InputEventMouseMotion and is_dragged:
-		global_position.x = event.global_position.x
-		if not is_instance_valid(container):
-			return
-		var container_pos := container.get_global_rect().position.x
-		var container_end := container.get_global_rect().end.x
-		global_position.x = clampf(global_position.x, container_pos, container_end)
-		var frame := roundi((global_position.x - container_pos) / KeyframeTimeline.frame_ui_size)
-		if frame >= Global.current_project.frames.size():
-			return
-		# Change frame
-		Global.current_project.selected_cels.clear()
-		var frame_layer := [frame, Global.current_project.current_layer]
-		if !Global.current_project.selected_cels.has(frame_layer):
-			Global.current_project.selected_cels.append(frame_layer)
+		_update_position(event.global_position)
 
-		Global.current_project.change_cel(frame, -1)
+
+func _update_position(new_pos: Vector2) -> void:
+	var container_pos := container.get_global_rect().position.x
+	var container_end := container.get_global_rect().end.x
+	global_position.x = clampf(new_pos.x, container_pos, container_end)
+	var frame := roundi((global_position.x - container_pos) / KeyframeTimeline.frame_ui_size)
+	if frame >= Global.current_project.frames.size():
+		return
+		# Change frame
+	Global.current_project.selected_cels.clear()
+	var frame_layer := [frame, Global.current_project.current_layer]
+	if !Global.current_project.selected_cels.has(frame_layer):
+		Global.current_project.selected_cels.append(frame_layer)
+	Global.current_project.change_cel(frame, -1)
 
 
 func _draw() -> void:
