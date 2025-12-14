5dde23af6b0bb2cd53f506efc9358ff378317c9f
Emmanouil Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2025-12-14 14:04:09 +0200

    Don't allow keyframes to go below frame 0
    Mostly so they can't be lost. The proper solution would be to support negative frames, like Godot's animation player editor does, but this should work for now.

--- src/UI/Timeline/KeyframeTimeline/KeyframeButton.gd
+++ src/UI/Timeline/KeyframeTimeline/KeyframeButton.gd
@@ -27,10 +27,11 @@ func _on_gui_input(event: InputEvent) -> void:
 	var selected_keyframe_buttons := KeyframeTimeline.get_selected_keyframe_buttons()
 	if not self in selected_keyframe_buttons:
 		return
+	var parent := get_parent() as Control
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
 			is_dragged = true
-			drag_mouse_start_pos = get_parent().get_local_mouse_position().x
+			drag_mouse_start_pos = parent.get_local_mouse_position().x
 			for keyframe_button in selected_keyframe_buttons:
 				keyframe_button.drag_pos = keyframe_button.position.x
 		else:
@@ -39,8 +40,10 @@ func _on_gui_input(event: InputEvent) -> void:
 				updated_position.emit()
 
 	if event is InputEventMouseMotion and is_dragged:
-		var mouse_pos: float = get_parent().get_local_mouse_position().x
+		var mouse_pos: float = parent.get_local_mouse_position().x
 		var delta := mouse_pos - drag_mouse_start_pos
 		for keyframe_button in selected_keyframe_buttons:
 			var new_pos := keyframe_button.drag_pos + delta
 			keyframe_button.position.x = snappedi(new_pos, KeyframeTimeline.frame_ui_size)
+			if keyframe_button.position.x < 0:
+				keyframe_button.position.x = 0
