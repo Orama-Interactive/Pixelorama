83e947e0e2c1a6e5c6582ccad0b9961d733149d4
Emmanouil Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2026-01-02 17:34:08 +0200

    Add support for animated layer effects and a keyframe-based timeline (#1417)
    * Start working on animatable layer effects
    
    * Interpolate colors & animated effects work on apply
    
    * Add nodes to change tween properties
    
    * Use smaller texts for the tween property option buttons
    
    * Fix crash when enabling animation, disabling and enabling it again
    
    * Remove keyframes
    
    * Load and save animated parameters in pxo files
    
    * Format
    
    * Start working on a keyframe based timeline
    
    * Create keyframes in the UI
    
    * Start working on keyframe value changing
    
    * Remove animated layer effect code from the layer FX settings
    
    * Refactor animated_params dictionary
    
    * Probably fix pxo loading
    
    At least old pxo files seem to be working properly
    
    * Update timeline when adding/removing keyframes
    
    * Use a tree for the layer elements
    
    Need to implement folding logic for the keyframes themselves though
    
    * Add a timeline cursor
    
    * Improve cursor moving logic
    
    * More timeline cursor improvements
    
    * Some UI improvements
    
    * Make timeline a bit prettier
    
    * Fix animated params pxo loading
    
    * Some small improvements
    
    * Fix scrolling
    
    * Don't create keyframe if there already is one
    
    * Fix frame container not resizing when clicking on keyframes
    
    * Added Constant transition
    
    * Add undo/redo when adding and deleting keyframes
    
    * Fix keyframe unselection
    
    * Start working on multiple selected keyframes
    
    * Change name of a script
    
    * Change properties of all selected keyframes
    
    With undo/redo support
    
    * [WIP] Move keyframes
    
    While it technically works, the UI side is not being updated. Also, I just realized that the way we're handling keyframe creation and deletion is wrong, because the nodes get freed when switching to a different layer, or a different project. So we most likely should just re-create the keyframe nodes when we add, delete or move them. Which means I also need to change the selected_keyframes array to store param_name and frame_index, instead of nodes.
    
    * Add IDs to keyframes for easier handling
    
    * Don't allow keyframes to go below frame 0
    
    Mostly so they can't be lost. The proper solution would be to support negative frames, like Godot's animation player editor does, but this should work for now.
    
    * Zoom in the timeline
    
    * Support booleans and colors, scroll timeline when changing cel or layer
    
    * Implement the keyframe timeline inside the main timeline panel
    
    I feel like the UI does need some improvements still
    
    * Make properties grid container and delete keyframe button persistent nodes
    
    * Undo/redo refreshes the keyframe property UI
    
    * Hide tags when keyframe timeline is visible
    
    * Fix property nodes not having the correct name
    
    * Use a custom node to draw frames instead of using labels
    
    * Fix group layers passthrough mode not having its effect be animated
    
    * Don't create tracks for properties that currently cannot be animated
    
    Such as textures
    
    * Better documentation and future-proofing

---
+++ src/UI/Timeline/KeyframeTimeline/KeyframeButton.gd
@@ -0,0 +1,49 @@
+class_name KeyframeButton
+extends TextureButton
+
+signal updated_position
+
+const KEYFRAME_ICON := preload("uid://yhha3l44svgs")
+const KEYFRAME_SELECTED_ICON := preload("uid://dtx6hygsgoifb")
+
+var keyframe_id := 0
+var dict: Dictionary
+var param_name: String
+var frame_index: int
+var is_dragged := false
+var drag_mouse_start_pos := 0.0
+var drag_pos := 0.0
+
+
+func _init() -> void:
+	add_to_group(&"KeyframeButtons")
+	toggle_mode = true
+	texture_normal = KEYFRAME_ICON
+	texture_pressed = KEYFRAME_SELECTED_ICON
+	gui_input.connect(_on_gui_input)
+
+
+func _on_gui_input(event: InputEvent) -> void:
+	var selected_keyframe_buttons := KeyframeTimeline.get_selected_keyframe_buttons()
+	if not self in selected_keyframe_buttons:
+		return
+	var parent := get_parent() as Control
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			is_dragged = true
+			drag_mouse_start_pos = parent.get_local_mouse_position().x
+			for keyframe_button in selected_keyframe_buttons:
+				keyframe_button.drag_pos = keyframe_button.position.x
+		else:
+			if is_dragged:
+				is_dragged = false
+				updated_position.emit()
+
+	if event is InputEventMouseMotion and is_dragged:
+		var mouse_pos: float = parent.get_local_mouse_position().x
+		var delta := mouse_pos - drag_mouse_start_pos
+		for keyframe_button in selected_keyframe_buttons:
+			var new_pos := keyframe_button.drag_pos + delta
+			keyframe_button.position.x = snappedi(new_pos, KeyframeTimeline.frame_ui_size)
+			if keyframe_button.position.x < 0:
+				keyframe_button.position.x = 0
