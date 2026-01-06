a6cbd675729806b5fecb0e07c5a95ba30a651016
Variable <77773850+Variable-ind@users.noreply.github.com>
2026-01-06 15:07:25 +0500

    Formatting

--- src/UI/Timeline/KeyframeTimeline/KeyframeButton.gd
+++ src/UI/Timeline/KeyframeTimeline/KeyframeButton.gd
@@ -15,13 +15,13 @@ var drag_mouse_start_pos := 0.0
 var drag_pos := 0.0
 
 
-
 func _init() -> void:
 	add_to_group(&"KeyframeButtons")
 	toggle_mode = true
 	texture_normal = KEYFRAME_ICON
 	texture_pressed = KEYFRAME_SELECTED_ICON
 	gui_input.connect(_on_gui_input)
+
 
 func _on_gui_input(event: InputEvent) -> void:
 	var selected_keyframe_buttons := KeyframeTimeline.get_selected_keyframe_buttons()
