847ad86341993509a5e3e52e8722682c1fd02a2d
Variable <77773850+Variable-ind@users.noreply.github.com>
2026-01-06 15:01:45 +0500

    To move single keyframe, you no longer have to select it first

--- src/UI/Timeline/KeyframeTimeline/KeyframeButton.gd
+++ src/UI/Timeline/KeyframeTimeline/KeyframeButton.gd
@@ -15,6 +15,7 @@ var drag_mouse_start_pos := 0.0
 var drag_pos := 0.0
 
 
+
 func _init() -> void:
 	add_to_group(&"KeyframeButtons")
 	toggle_mode = true
@@ -22,12 +23,24 @@ func _init() -> void:
 	texture_pressed = KEYFRAME_SELECTED_ICON
 	gui_input.connect(_on_gui_input)
 
-
 func _on_gui_input(event: InputEvent) -> void:
 	var selected_keyframe_buttons := KeyframeTimeline.get_selected_keyframe_buttons()
-	if not self in selected_keyframe_buttons:
-		return
 	var parent := get_parent() as Control
+	if not self in selected_keyframe_buttons:
+		# Check if it was pressed just recently, if it is then we have to reset
+		# selected_keyframe_buttons
+		if (
+			(
+				event is InputEventMouseButton
+				and event.button_index == MOUSE_BUTTON_LEFT
+				and event.pressed
+				and get_rect().has_point(parent.get_local_mouse_position())
+			)
+			or is_dragged
+		):
+			selected_keyframe_buttons = [self]
+		else:
+			return
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
 			is_dragged = true
@@ -37,6 +50,12 @@ func _on_gui_input(event: InputEvent) -> void:
 		else:
 			if is_dragged:
 				is_dragged = false
+				# if the button was clicked to drag, while it wasn't selected then the
+				# pressed signal isn't emitted yet (it emits on release), so we have to manually
+				# emit it first
+				if not button_pressed and self in selected_keyframe_buttons:
+					button_pressed = true
+					pressed.emit()
 				updated_position.emit()
 
 	if event is InputEventMouseMotion and is_dragged:
