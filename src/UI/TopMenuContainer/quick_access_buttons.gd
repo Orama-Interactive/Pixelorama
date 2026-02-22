aba0cc9473b467b0dbdce5e8fef2c741e2368346
Emmanouil Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2026-02-22 02:34:48 +0200

    Add quick access buttons for mobile
    Buttons for save, undo, redo, copy, cut, paste & delete. I also want to add Ctrl, Shift & Alt buttons there. Supercedes #1431

---
+++ src/UI/TopMenuContainer/quick_access_buttons.gd
@@ -0,0 +1,36 @@
+extends VFlowContainer
+
+@onready var top_menu_container: Panel = $"../../.."
+
+
+func _ready() -> void:
+	if DisplayServer.is_touchscreen_available():
+		show()
+
+
+func _on_save_pressed() -> void:
+	top_menu_container.file_menu_id_pressed(Global.FileMenu.SAVE)
+
+
+func _on_undo_pressed() -> void:
+	top_menu_container.edit_menu_id_pressed(Global.EditMenu.UNDO)
+
+
+func _on_redo_pressed() -> void:
+	top_menu_container.edit_menu_id_pressed(Global.EditMenu.REDO)
+
+
+func _on_copy_pressed() -> void:
+	top_menu_container.edit_menu_id_pressed(Global.EditMenu.COPY)
+
+
+func _on_cut_pressed() -> void:
+	top_menu_container.edit_menu_id_pressed(Global.EditMenu.CUT)
+
+
+func _on_paste_pressed() -> void:
+	top_menu_container.edit_menu_id_pressed(Global.EditMenu.PASTE)
+
+
+func _on_delete_pressed() -> void:
+	top_menu_container.edit_menu_id_pressed(Global.EditMenu.DELETE)
