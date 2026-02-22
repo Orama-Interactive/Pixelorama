b1e8717f53c4a0f9b5992559aa75c61dc914434b
Emmanouil Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2026-02-22 15:58:44 +0200

    Add Shift, Ctrl & Alt quick access buttons for mobile

--- src/UI/TopMenuContainer/quick_access_buttons.gd
+++ src/UI/TopMenuContainer/quick_access_buttons.gd
@@ -34,3 +34,24 @@ func _on_paste_pressed() -> void:
 
 func _on_delete_pressed() -> void:
 	top_menu_container.edit_menu_id_pressed(Global.EditMenu.DELETE)
+
+
+func _on_shift_toggled(toggled_on: bool) -> void:
+	var event := InputEventKey.new()
+	event.keycode = KEY_SHIFT
+	event.pressed = toggled_on
+	Input.parse_input_event(event)
+
+
+func _on_ctrl_toggled(toggled_on: bool) -> void:
+	var event := InputEventKey.new()
+	event.keycode = KEY_CTRL
+	event.pressed = toggled_on
+	Input.parse_input_event(event)
+
+
+func _on_alt_toggled(toggled_on: bool) -> void:
+	var event := InputEventKey.new()
+	event.keycode = KEY_ALT
+	event.pressed = toggled_on
+	Input.parse_input_event(event)
