cb1c32adbcd165ebf207b7ee3db1c52a3af1e266
Variable-ind <77773850+Variable-ind@users.noreply.github.com>
2026-04-23 15:36:51 +0500

    Use Integer values in shader (this makes the layer effect values also be like the inage effect dialog). Use the hsv to rgb converters from godot's source code instead

--- src/UI/Dialogs/ImageEffects/HSVDialog.gd
+++ src/UI/Dialogs/ImageEffects/HSVDialog.gd
@@ -34,12 +34,9 @@ func commit_action(cel: Image, project := Global.current_project) -> void:
 		var selection := project.selection_map.return_cropped_copy(project, project.size)
 		selection_tex = ImageTexture.create_from_image(selection)
 
-	hue = remap(hue, -180, 180, -1, 1)
-	sat = remap(sat, -100, 100, -1, 1)
-	val = remap(val, -100, 100, -1, 1)
 	var params := {
 		"hue": hue,
-		"saturation": 0.1,
+		"saturation": sat,
 		"value": val,
 		"selection": selection_tex,
 		"wrap_overflowing": overflow_check_box.button_pressed
@@ -49,16 +46,7 @@ func commit_action(cel: Image, project := Global.current_project) -> void:
 			preview.material.set_shader_parameter(param, params[param])
 	else:
 		var gen := ShaderImageEffect.new()
-		print("======================")# 0.09803921729326
-		var old_a = cel.get_pixel(1, 1).g
-		var old_b = cel.get_pixel(2, 1).g
-		var old_c = cel.get_pixel(3, 1).g
 		gen.generate_image(cel, shader, params, project.size)
-		var new_a = cel.get_pixel(1, 1).g
-		var new_b = cel.get_pixel(2, 1).g
-		var new_c = cel.get_pixel(3, 1).g
-		prints("New:", new_a, new_b, new_c)
-		#prints("Dif:", new_a - old_a, new_b - old_b, new_c - old_c)
 
 
 func _reset() -> void:
