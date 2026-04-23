f3fb069febf44ada16a56347dfafd1cadb952f68
Variable-ind <77773850+Variable-ind@users.noreply.github.com>
2026-04-23 14:29:16 +0500

    tinkering with code

--- src/UI/Dialogs/ImageEffects/HSVDialog.gd
+++ src/UI/Dialogs/ImageEffects/HSVDialog.gd
@@ -26,17 +26,20 @@ func _about_to_popup() -> void:
 
 
 func commit_action(cel: Image, project := Global.current_project) -> void:
-	var hue = animate_panel.get_animated_value(commit_idx, Animate.HUE) / 360
-	var sat = animate_panel.get_animated_value(commit_idx, Animate.SATURATION) / 100
-	var val = animate_panel.get_animated_value(commit_idx, Animate.VALUE) / 100
+	var hue = animate_panel.get_animated_value(commit_idx, Animate.HUE)
+	var sat = animate_panel.get_animated_value(commit_idx, Animate.SATURATION)
+	var val = animate_panel.get_animated_value(commit_idx, Animate.VALUE)
 	var selection_tex: ImageTexture
 	if selection_checkbox.button_pressed and project.has_selection:
 		var selection := project.selection_map.return_cropped_copy(project, project.size)
 		selection_tex = ImageTexture.create_from_image(selection)
 
+	hue = remap(hue, -180, 180, -1, 1)
+	sat = remap(sat, -100, 100, -1, 1)
+	val = remap(val, -100, 100, -1, 1)
 	var params := {
 		"hue": hue,
-		"saturation": sat,
+		"saturation": 0.1,
 		"value": val,
 		"selection": selection_tex,
 		"wrap_overflowing": overflow_check_box.button_pressed
@@ -46,7 +49,16 @@ func commit_action(cel: Image, project := Global.current_project) -> void:
 			preview.material.set_shader_parameter(param, params[param])
 	else:
 		var gen := ShaderImageEffect.new()
+		print("======================")# 0.09803921729326
+		var old_a = cel.get_pixel(1, 1).g
+		var old_b = cel.get_pixel(2, 1).g
+		var old_c = cel.get_pixel(3, 1).g
 		gen.generate_image(cel, shader, params, project.size)
+		var new_a = cel.get_pixel(1, 1).g
+		var new_b = cel.get_pixel(2, 1).g
+		var new_c = cel.get_pixel(3, 1).g
+		prints("New:", new_a, new_b, new_c)
+		#prints("Dif:", new_a - old_a, new_b - old_b, new_c - old_c)
 
 
 func _reset() -> void:
