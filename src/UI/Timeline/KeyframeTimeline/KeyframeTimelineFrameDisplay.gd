d09c0094a05c40abee6f12d60662f18e21da4474
Variable-ind <77773850+Variable-ind@users.noreply.github.com>
2026-08-15 19:40:17 +0500

    Make frame text bigger in Keyframe timeline if it is the current frame

--- src/UI/Timeline/KeyframeTimeline/KeyframeTimelineFrameDisplay.gd
+++ src/UI/Timeline/KeyframeTimeline/KeyframeTimelineFrameDisplay.gd
@@ -12,6 +12,12 @@ func _draw() -> void:
 	var font := Themes.get_font()
 	var project := Global.current_project
 	for i in project.frames.size():
-		var xx := i * KeyframeTimeline.frame_ui_size - x_offset
-		draw_line(Vector2(xx, 0), Vector2(xx, size.y), Color.WHITE)
-		draw_string(font, Vector2(xx + 2, 16), str(i + 1))
+		# NOTE: We use maxf(1, ...) so that the line next to frame 1 is drawn properly.
+		var xx := maxf(1, i * KeyframeTimeline.frame_ui_size - x_offset)
+		var width: int = -1
+		if i == project.current_frame:
+			width = 2
+		draw_line(Vector2(xx, 0), Vector2(xx, size.y), Color.WHITE, width)
+		draw_string(
+			font, Vector2(xx + 2, 16), str(i + 1), HORIZONTAL_ALIGNMENT_LEFT, -1, 16 + width
+		)
